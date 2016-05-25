c99b0f9dbbbfbe1e6d8ce20c5aca3dff72108a78
annaship <tetya-loshad@yandex.ru>
2016-05-25 16:19:28 -0400

    update_refids works

---
+++ update_refids.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import util
+
+if __name__ == '__main__':
+  vampsdev_vamps_mysql_util = util.Mysql_util(host = "vampsdev", db = "vamps", read_default_group = "clientservers")
+  query = "show tables"
+  a = vampsdev_vamps_mysql_util.execute_fetch_select(query)
+  print "AAA"
+  print a
+
+"""
+incremantal
+
+1)
+create table refids_per_dataset_temp
+(
+  refids_per_dataset_id int unsigned NOT NULL AUTO_INCREMENT primary key,
+  frequency double NOT NULL DEFAULT '0' COMMENT 'sum seq_count (for this seq/project/dataset across all lines and runs) divided by dataset_count',
+  project varchar(32) NOT NULL,
+  dataset varchar(64) NOT NULL DEFAULT '',
+  refhvr_ids text NOT NULL,
+  seq_count int(11) unsigned NOT NULL COMMENT 'sum seq_count for this seq/project/dataset across all lines and runs',
+  distance decimal(7,5) DEFAULT NULL COMMENT 'gast_distance AS distance',
+  rep_id int(10) unsigned NOT NULL COMMENT 'sequence_pdr_info_ill_id AS rep_id',
+  dataset_count mediumint(8) unsigned NOT NULL COMMENT 'number of reads in the dataset',
+  dataset_id smallint(5) unsigned NOT NULL,
+  project_id mediumint(5) unsigned NOT NULL,
+  UNIQUE KEY rep_id (rep_id),
+  key dataset (dataset),
+  key project (project)
+)
+
+insert ignore into refids_per_dataset_temp (frequency, project, dataset, refhvr_ids, seq_count, distance, rep_id, dataset_count)
+select v.frequency, v.project, v.dataset, v.refhvr_ids, v.seq_count, v.distance, v.rep_id, v.dataset_count
+  from vamps_sequences_transfer_temp v
+  LEFT JOIN refids_per_dataset USING(rep_id)
+  WHERE refids_per_dataset.rep_id IS null
+no ignore: ERROR 1062 (23000): Duplicate entry '0' for key 'rep_id'
+taiga screen
+running
+date
+Wed May 25 10:45:37 EDT 2016
+Query OK, 5187270 rows affected, 2 warnings (34 min 58.52 sec)
+| Warning | 1364 | Field 'dataset_id' doesn't have a default value |
+| Warning | 1364 | Field 'project_id' doesn't have a default value |
+
+
+update refids_per_dataset_temp
+  join new_dataset using(dataset)
+  set refids_per_dataset_temp.dataset_id = new_dataset.dataset_id;
+Query OK, 5187270 rows affected (1 min 8.08 sec)
+
+update refids_per_dataset_temp
+  join new_project using(project)
+  set refids_per_dataset_temp.project_id = new_project.project_id;
+Query OK, 5187270 rows affected (57.56 sec)
+
+module load mysql/5.6.12
+time mysqldump -h vampsdb vamps refids_per_dataset | gzip > /users/ashipunova/backups/refids_per_dataset.`date '+%m%d%y'`.sql.gz 
+real	11m43.972s
+
+create refids_per_dataset_previous instead
+
+insert ignore into refids_per_dataset (frequency, seq_count, distance, rep_id, dataset_count, dataset_id, project_id)
+  select frequency, seq_count, distance, rep_id, dataset_count, dataset_id, project_id
+  from refids_per_dataset_temp
+Query OK, 5187270 rows affected (2 min 24.65 sec)
+
+2)
+cd /workspace/ashipunova/refhvrid
+time mysql -h vampsdb vamps -e "SELECT rep_id, refhvr_ids FROM refids_per_dataset_temp" | tail -n+2 >rep_id_refhvr_ids_temp.csv
+real	0m9.946s
+
+create table rep_id_refhvr_id_temp
+(
+  rep_id_refhvr_id_id int(10) unsigned NOT NULL AUTO_INCREMENT primary key,
+  rep_id int(10) unsigned NOT NULL COMMENT 'sequence_pdr_info_ill_id AS rep_id',
+  refhvr_id varchar(16) NOT NULL,
+  UNIQUE KEY rep_id_refhvr (rep_id, refhvr_id)
+)
+
+/workspace/ashipunova/refhvrid$ 
+time cat rep_id_refhvr_ids_temp.csv | tail -n+2 >rep_id_refhvr_ids_temp1.csv
+real	0m46.172s
+
+time python separate_refids.py rep_id_refhvr_ids_temp1.csv > rep_id_refhvr_ids_temp_separated.csv; mail_done
+real	0m22.348s
+
+time mysql -h vampsdb vamps -e "LOAD DATA LOCAL INFILE '/workspace/ashipunova/refhvrid/rep_id_refhvr_ids_temp_separated.csv' IGNORE INTO TABLE rep_id_refhvr_id  FIELDS TERMINATED BY ',' (rep_id, refhvr_id);"; mail_done
+real	1m17.469s
+
+"""
---
+++ util.py
@@ -0,0 +1,204 @@
+import os
+import MySQLdb
+import logging
+
+class Mysql_util:
+    """
+    Connection to vamps or vampsdev
+
+    Takes parameters from ~/.my.cnf, default host = "vampsdev", db = "vamps"
+    if different use my_conn = Mysql_util(host, db, read_default_file, port)
+    """
+    # def __init__(self, host = "bpcweb7", db = "vamps", read_default_file = os.path.expanduser("~/.my.cnf"), port = 3306):
+    def __init__(self, host = "vampsdev", db = "test", read_default_group = "client", read_default_file = os.path.expanduser("~/.my.cnf")):
+        self.utils     = Utils()
+        self.conn      = None
+        self.cursor    = None
+        self.rows      = 0
+        self.new_id    = None
+        self.lastrowid = None
+        self.rowcount  = None
+        
+        if read_default_file == "":
+          if self.utils.is_local():
+            read_default_file = os.path.expanduser("~/.my.cnf_local")
+        # print "read_default_file = %s" % read_default_file
+
+        try:
+            self.utils.print_both("=" * 40)
+            self.utils.print_both("host = " + str(host) + ", db = "  + str(db) + ", read_default_group = " + str(read_default_group)  + ", read_default_file = " + str(read_default_file))
+            self.utils.print_both("=" * 40)
+
+            # self.conn = MySQLdb.connect(host = host, db = db, read_default_file = read_default_file, port = port)
+            self.conn    =  MySQLdb.connect(host = host, db = db, read_default_group = read_default_group, read_default_file = "~/.my.cnf")
+            print "host = %s, db = %s, read_default_file = %s" % (host, db, read_default_file)
+            
+            self.cursor = self.conn.cursor()
+
+        except MySQLdb.Error, e:
+            self.utils.print_both("Error %d: %s" % (e.args[0], e.args[1]))
+            raise
+        except:                       # catch everything
+            self.utils.print_both("Unexpected:")
+            self.utils.print_both(sys.exc_info()[0])
+            raise                       # re-throw caught exception
+
+    def execute_fetch_select(self, sql):
+      # print "+" * 20
+      # print sql
+      if self.cursor:
+        try:
+          self.cursor.execute(sql)
+          res         = self.cursor.fetchall ()
+          field_names = [i[0] for i in self.cursor.description]
+        except:
+          self.utils.print_both(("ERROR: query = %s") % sql)
+          raise
+        return (res, field_names)
+
+    def execute_no_fetch(self, sql):
+      if self.cursor:
+          self.cursor.execute(sql)
+          self.conn.commit()
+          return self.cursor.lastrowid
+
+    def execute_insert(self, table_name, field_name, val_list, ignore = "IGNORE"):
+      try:
+        sql = "INSERT %s INTO %s (%s) VALUES (%s)" % (ignore, table_name, field_name, val_list)
+
+        if self.cursor:
+          self.cursor.execute(sql)
+          self.conn.commit()
+          return (self.cursor.rowcount, self.cursor.lastrowid)
+      except:
+        self.utils.print_both(("ERROR: query = %s") % sql)
+        raise
+
+    def get_all_name_id(self, table_name, id_name = "", field_name = "", where_part = ""):
+      if (field_name == ""):
+        field_name = table_name
+      if (id_name == ""):
+        id_name = table_name + '_id'
+      my_sql  = """SELECT %s, %s FROM %s %s""" % (field_name, id_name, table_name, where_part)
+      # self.utils.print_both(("my_sql from get_all_name_id = %s") % my_sql)
+      res     = self.execute_fetch_select(my_sql)
+      if res:
+        return res[0]
+
+    def execute_simple_select(self, field_name, table_name, where_part):
+      id_query  = "SELECT %s FROM %s %s" % (field_name, table_name, where_part)
+      return self.execute_fetch_select(id_query)[0]
+
+    def get_id(self, field_name, table_name, where_part, rows_affected = [0,0]):
+      # self.utils.print_array_w_title(rows_affected, "=====\nrows_affected from def get_id")
+
+      if rows_affected[1] > 0:
+        id_result = int(rows_affected[1])
+      else:
+        try:
+          # id_query  = "SELECT %s FROM %s %s" % (field_name, table_name, where_part)
+          id_result_full = self.execute_simple_select(field_name, table_name, where_part)
+          id_result = int(id_result_full[0][0])
+        except:
+          self.utils.print_both("Unexpected:")
+          # self.utils.print_both(sys.exc_info()[0])
+          raise
+
+      # self.utils.print_array_w_title(id_result, "=====\nid_result IN get_id")
+      return id_result
+
+
+class Utils:
+    def __init__(self):
+        pass
+
+    def is_local(self):
+        print os.uname()[1]
+
+
+        dev_comps = ['ashipunova.mbl.edu', "as-macbook.home", "as-macbook.local", "Ashipunova.local", "Annas-MacBook-new.local", "Annas-MacBook.local",'Andrews-Mac-Pro.local']
+
+
+        if os.uname()[1] in dev_comps:
+            return True
+        else:
+            return False
+
+    def is_vamps(self):
+        print os.uname()[1]
+        dev_comps = ['bpcweb8','bpcweb7','bpcweb7.bpcservers.private', 'bpcweb8.bpcservers.private']
+        if os.uname()[1] in dev_comps:
+            return True
+        else:
+            return False
+            
+    def is_vamps_prod(self):
+        print os.uname()[1]
+        dev_comps = ['bpcweb8', 'bpcweb8.bpcservers.private']
+        if os.uname()[1] in dev_comps:
+            return True
+        else:
+            return False
+            
+    def print_both(self, message):
+        print message
+        logging.debug(message)
+
+    def print_array_w_title(self, message, title = 'message'):
+      print title
+      print message
+
+    def read_csv_into_list(self, file_name):
+      csv_file_content_all = list(csv.reader(open(file_name, 'rb'), delimiter = ','))
+      csv_file_fields      = csv_file_content_all[0]
+      csv_file_content     = csv_file_content_all[1:]
+      return (csv_file_fields, csv_file_content)
+
+    def flatten_2d_list(self, list):
+      return [item for sublist in list for item in sublist]
+
+    def wrapper(self, func, *args, **kwargs):
+        def wrapped():
+            return func(*args, **kwargs)
+        return wrapped
+
+    def benchmarking(self, func, func_name, *args, **kwargs):
+      print "START %s" % func_name
+      wrapped  = utils.wrapper(func, *args)
+      time_res = timeit.timeit(wrapped, number = 1)
+      print 'time: %.2f s' % time_res
+      # USE: utils.benchmarking(pr.parse_project_csv, "parse_project_csv", project_csv_file_name)
+
+    def search_in_2d_list(self, search, data):
+      for sublist in data:
+        if search in sublist:
+          return sublist
+          break
+
+    def find_val_in_nested_list(self, hey, needle):
+      return [v for k, v in hey if k.lower() == needle.lower()]
+
+    def find_key_by_value_in_dict(self, hey, needle):
+      return [k for k, v in hey if v == needle]
+
+    def make_entry_w_fields_dict(self, fields, entry):
+      return dict(zip(fields, entry))
+
+    def write_to_csv_file(self, file_name, res, file_mode = "wb"):
+      data_from_db, field_names = res
+      # print "VVVV"
+      # print field_names
+
+      with open(file_name, file_mode) as csv_file:
+        csv_writer = csv.writer(csv_file)
+        if file_mode == "wb":
+          csv_writer.writerow(field_names) # write headers
+        csv_writer.writerows(data_from_db)    
+
+    def get_csv_file_calls(self, query):
+      return prod_mysql_util.execute_fetch_select(query)
+      # prod_mysql_util = Mysql_util(host = host_prod, db = "vamps", read_default_file = read_default_file_prod, port = port_prod)
+      
+    def slicedict(self, my_dict, key_list):
+      return {k: v for k, v in my_dict.items() if k in key_list}
+      
