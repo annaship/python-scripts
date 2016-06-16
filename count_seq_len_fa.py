bad7da9b6fc775521b03a786526ee224541c3718
annaship <tetya-loshad@yandex.ru>
2016-06-16 16:31:23 -0400

    class

--- count_seq_len_fa.py
+++ count_seq_len_fa.py
@@ -1,54 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ver 2 (class)
+
 import IlluminaUtils.lib.fastalib as fa
 import os
 import sys
 
-usage = """python count_seq_len_fa.py DIRNAME [-ve]"""
-
-def get_files(walk_dir_name, ext = ""):
-    files = {}
-    filenames = []
-    for dirname, dirnames, filenames in os.walk(walk_dir_name, followlinks=True):
-        if ext:
-            filenames = [f for f in filenames if f.endswith(ext)]
-        
-        for file_name in filenames:
-            full_name = os.path.join(dirname, file_name)
-            (file_base, file_extension) = os.path.splitext(os.path.join(dirname, file_name))
-            files[full_name] = (dirname, file_base, file_extension)
-    return files
-
-def check_if_verb():
-  try: 
-    if sys.argv[2] == "-ve":
-      return True
-  except IndexError:
-    return False
-  except: 
-    print "Unexpected error:", sys.exc_info()[0]
-    return False
-  return False
-
-
-start_dir = sys.argv[1]
-print "Start from %s" % start_dir
-print "Getting file names"
-
-all_dirs = set()
-
-#fa_files = get_files("/xraid2-2/sequencing/Illumina", ".fastq.gz")
-# "/xraid2-2/sequencing/Illumina/20151014ns"
-fa_files = get_files(start_dir, ".fa")
-print "Found %s fa files" % (len(fa_files))
-
-check_if_verb = check_if_verb()
-
-for file_name in fa_files:
-  if (check_if_verb):
-    print file_name
-
-  try:
-    f_input  = fa.ReadFasta(file_name)
+class Fa_seq_len:
+  def __init__(self):
+    self.usage = """python count_seq_len_fa.py DIRNAME [-ve]"""
+    self.all_dirs = set()
     
+  def get_files(self, walk_dir_name, ext = ""):
+      files = {}
+      filenames = []
+      for dirname, dirnames, filenames in os.walk(walk_dir_name, followlinks=True):
+          if ext:
+              filenames = [f for f in filenames if f.endswith(ext)]
+        
+          for file_name in filenames:
+              full_name = os.path.join(dirname, file_name)
+              (file_base, file_extension) = os.path.splitext(os.path.join(dirname, file_name))
+              files[full_name] = (dirname, file_base, file_extension)
+      return files
+
+  def check_if_verb(self):
+    try: 
+      if sys.argv[2] == "-ve":
+        return True
+    except IndexError:
+      return False
+    except: 
+      print "Unexpected error:", sys.exc_info()[0]
+      return False
+    return False
+
+  def get_seq_len_distrib(self, f_input):
+    for idx, seq in enumerate(f_input.sequences):
+      seq_len = len(seq)
+      print seq_len
+  
+  def print_short_seq(self, f_input):
     for idx, seq in enumerate(f_input.sequences):
       seq_len = len(seq)
       if seq_len < 200:
@@ -56,15 +48,83 @@ for file_name in fa_files:
         print seq
         print "WARNING, sequence length in %s = %s. It's less than 200!" % (file_name, seq_len)
         all_dirs.add(fa_files[file_name][0])
-  except RuntimeError:
+    return all_dirs
+  
+
+  # start_dir = sys.argv[1]
+  # print "Start from %s" % start_dir
+  # print "Getting file names"
+
+
+  #fa_files = get_files("/xraid2-2/sequencing/Illumina", ".fastq.gz")
+  # "/xraid2-2/sequencing/Illumina/20151014ns"
+  # fa_files = self.get_files(start_dir, ".fa")
+  # print "Found %s fa files" % (len(fa_files))
+
+  # check_if_verb = self.check_if_verb()
+
+  # for file_name in fa_files:
+  #   if (check_if_verb):
+  #     print file_name
+  #
+  #   try:
+  #     f_input  = fa.ReadFasta(file_name)
+  #
+  #     # for idx, seq in enumerate(f_input.sequences):
+  #     #   seq_len = len(seq)
+  #     #   if seq_len < 200:
+  #     #     print f_input.ids[idx]
+  #     #     print seq
+  #     #     print "WARNING, sequence length in %s = %s. It's less than 200!" % (file_name, seq_len)
+  #     #     all_dirs.add(fa_files[file_name][0])
+  #   except RuntimeError:
+  #     if (check_if_verb):
+  #       print sys.exc_info()[0]
+  #   except:
+  #     print "Unexpected error:", sys.exc_info()[0]
+  #     raise
+  #     next
+  #
+  # print "Current directory:"
+  # print all_dirs
+
+
+if __name__ == '__main__':
+  seq_len = Fa_seq_len()
+  
+  start_dir = sys.argv[1]
+  print "Start from %s" % start_dir
+  print "Getting file names"
+  
+  fa_files = seq_len.get_files(start_dir, ".fa")
+  print "Found %s fa files" % (len(fa_files))
+  
+  check_if_verb = seq_len.check_if_verb()
+  
+  for file_name in fa_files:
     if (check_if_verb):
-      print sys.exc_info()[0]
-  except:
-    print "Unexpected error:", sys.exc_info()[0]
-    raise
-    next
+      print file_name
 
-print "Current directory:"
-print all_dirs
+    all_dirs = seq_len.all_dirs
+    
+    try:
+      f_input  = fa.ReadFasta(file_name)
+    
+      for idx, seq in enumerate(f_input.sequences):
+        seq_len = len(seq)
+        if seq_len < 200:
+          print f_input.ids[idx]
+          print seq
+          print "WARNING, sequence length in %s = %s. It's less than 200!" % (file_name, seq_len)
+          all_dirs.add(fa_files[file_name][0])
+    except RuntimeError:
+      if (check_if_verb):
+        print sys.exc_info()[0]
+    except:
+      print "Unexpected error:", sys.exc_info()[0]
+      raise
+      next
 
-
+  print "Current directory:"
+  print all_dirs
+  
