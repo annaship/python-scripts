1e65c1e38a03eff0b8f3702c61490fb3eb59b504
annaship <tetya-loshad@yandex.ru>
2016-10-28 14:03:59 -0400

    counts

--- go-obo-parser_my.py
+++ go-obo-parser_my.py
@@ -10,6 +10,7 @@ added database utility by Anna Shipunova 2016-10-27
 from __future__ import with_statement
 from collections import defaultdict
 import itertools
+import os
 
 __author__    = "Uli Koehler"
 __copyright__ = "Copyright 2013 Uli Koehler"
@@ -25,12 +26,12 @@ def processGOTerm(goTerm):
     for key, value in ret.iteritems():
         if len(value) == 1:
             ret[key] = value[0]
-    return ret    
+    return ret
 
 def parseGOOBO(filename):
     """
     Parses a Gene Ontology dump in OBO v1.2 format.
-    Yields each 
+    Yields each
     Keyword arguments:
         filename: The filename to read
     """
@@ -54,17 +55,17 @@ def parseGOOBO(filename):
         #Add last term
         if currentGOTerm is not None:
             yield processGOTerm(currentGOTerm)
-            
+
 def create_insert_term_query(goTerm):
-    """ 
+    """
     insert into term (ontology_id, term_name, identifier, definition, is_obsolete, is_root_term, is_leaf)
     (1, )
-    
+
         {'is_a': ['CHEBI:25585 ! nonmetal atom', 'CHEBI:33300 ! pnictogen'], 'id': 'CHEBI:25555', 'name': 'nitrogen atom'}
-    
+
     """
     insert_term_query_1 = ""
-    
+
     try:
         term_name = goTerm['name']
         identifier = goTerm['id']
@@ -72,27 +73,27 @@ def create_insert_term_query(goTerm):
             definition = clean_definition(goTerm['def'])
         else:
             definition = ""
-            
+
         if ('is_obsolete' in goTerm) and (goTerm['is_obsolete'] == "true") :
             is_obsolete = 1
         else:
             is_obsolete = 0
-        
+
         if 'is_a' in goTerm:
             is_root_term = 0
             is_leaf      = 1
         else:
             is_root_term = 1
             is_leaf      = 0
-            
-    
+
+
         insert_term_query_1 = """(2, "%s", "%s", "%s", "%s", "%s", "%s")\n""" % (term_name, identifier, definition, is_obsolete, is_root_term, is_leaf)
     except KeyError:
         pass
     except:
         raise
     return insert_term_query_1
-    
+
 def get_term_path(goTerm, parents):
     term_identifier = goTerm['id']
     # 'is_a': ['CHEBI:25585 ! nonmetal atom', 'CHEBI:33300 ! pnictogen']
@@ -109,23 +110,48 @@ def get_term_path(goTerm, parents):
             parents[term_identifier].append(ent.split(' ! '))
     # print "PPP parents"
     # print parents
-        
+
 def clean_definition(definition):
     cl_def = definition.strip(' []').replace('"', '').replace('\\', '')
     return ''.join([i if ord(i) < 128 else ' ' for i in cl_def])
-    
+
+def write_file(header, query, target):
+
+    target.write(header)
+    target.write("\n")
+    target.write(query)
+    print "target.tell()"
+    print target.tell()
+    target.close()
+
 def combine_insert_term_query(all_term_dict_l):
-    insert_term_query = """
-    insert into term (ontology_id, term_name, identifier, definition, is_obsolete, is_root_term, is_leaf)
-      values 
+    insert_term_query = ""
+    first_line = """
+    INSERT IGNORE INTO term (ontology_id, term_name, identifier, definition, is_obsolete, is_root_term, is_leaf)
+      VALUES
     """
-    cnts = 0    
+    cnts = 0
+    cnts_max = 0
+    max_lines = 50
+    i = 0
+    
     for goTerm in all_term_dict_l:
         # print goTerm
+        
         cnts += 1
+        cnts_max += 1
         insert_term_query += create_insert_term_query(goTerm)
+        if cnts_max == max_lines:
+            while os.path.exists("out%s.sql" % i):
+                i += 1
+
+            target = open("out%s.sql" % i, "w")
+            
+            write_file(first_line, insert_term_query, target)
+            cnts_max = 0
         if cnts < len(all_term_dict_l):
             insert_term_query += ", "
+            next
     return insert_term_query
 
 if __name__ == "__main__":
@@ -136,17 +162,17 @@ if __name__ == "__main__":
     args = parser.parse_args()
     all_term_dict = parseGOOBO(args.infile)
     all_term_dict_l = list(all_term_dict)
-    
+
     insert_term_query = combine_insert_term_query(all_term_dict_l)
-        
-    
+
+
     parents = {}
-    
+
     for goTerm in all_term_dict_l:
         # print goTerm
-        
+
         if 'is_a' in goTerm:
             get_term_path(goTerm, parents)
-    
+
     # print "NNN insert_term_query = %s" % insert_term_query
-    print insert_term_query
+    # print insert_term_query
