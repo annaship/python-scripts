6871e651a5479d7cd2a7fd4d7863cdbbf37de11d
annaship <tetya-loshad@yandex.ru>
2017-01-30 11:12:21 -0500

    print binomial

--- hlsu_tax.py
+++ hlsu_tax.py
@@ -19,10 +19,22 @@ class Taxonomy:
         return [taxon_split.append("NA") for i in range(8) if (len(taxon_split) < i+1)]
 
     def get_binomial(self, taxon_split):
-        print "taxon_split from get_binomial"
-        print taxon_split
+        print "taxon_split[4] from get_binomial"
+        print taxon_split[4]
+        
         try:
-            return taxon_split[5] + "_" + taxon_split[6]
+            print "from get_binomial"
+            print "superkingdom (%s)\nclass (%s)\norder (%s)\nfamily (%s)\ngenus (%s)\nbinomial (%s)" % (taxon_split[0], taxon_split[1], taxon_split[2], taxon_split[3], taxon_split[4], taxon_split[5])
+            
+            binomial_all = taxon_split[5].split("_")
+            genus = binomial_all[0]
+            if taxon_split[4] != genus:
+                print "OHOHOH taxon_split[4] (%s) != genus (%s)" % (taxon_split[4], genus)
+                
+            species = binomial_all[1]
+            strain = " ".join(binomial_all[2:])
+            print "genus = %s, species = %s, strain = %s" % (genus, species, strain)
+            return genus, species, strain
         except IndexError:
             return "NA"
         except:
@@ -37,27 +49,28 @@ class Taxonomy:
             raise
         
     
-    def format_header(self, header):
+    def format_header(self, line):
         # print header LSUcultures334_taxonomy.txt
         # AF289038	Eukaryota;Haptophyceae;Prymnesiales;Prymnesiaceae;Prymnesium;Prymnesium_parvum_f._patelliferum
         # goal:
         # AF289038	Eukaryota;Haptophyceae;Prymnesiales;Prymnesiaceae;Prymnesium;parvum;f._patelliferum
         
-        id, taxon = header.split("\t")
-        print "id = %s" % id
-        print "taxon = %s" % taxon
+        ref_id, taxon_string = line.split()
+        print "ref_id = %s" % ref_id
+        print "taxon_string = %s" % taxon_string
         
-        taxon_split = taxon.split(";")
+        taxon_split = taxon_string.split(";")
         print "len(taxon_split)"
         print len(taxon_split)
 
-        binomial = self.get_binomial(taxon_split)
+        # binomial = 
+        self.get_binomial(taxon_split)
         # print "binomial = %s" % binomial
-        
-        genus = self.get_genus(taxon_split)            
+        #
+        # genus = self.get_genus(taxon_split)
         # print "genus = %s" % genus
             
-        self.fill_out_empty_ranks(taxon_split)
+        # self.fill_out_empty_ranks(taxon_split)
 
         # print taxon_split[:5]
         
@@ -73,7 +86,7 @@ class Taxonomy:
         # print "reverse_from_family = "
         # print reverse_from_family
 
-        return ">" + id + "\t" + binomial + "\t" + genus + "\tNA\t" + reverse_from_family
+        # return ">" + id + "\t" + binomial + "\t" + genus + "\tNA\t" + reverse_from_family
         # "\t".join(reverse_from_family)
 
     def parse_taxonomy(self, args):
@@ -84,13 +97,7 @@ class Taxonomy:
                 # current_output_text = ""
                 line = line.strip()
                 if not line: continue #Skip empty
-                print "line.split() in parse_taxonomy"
-                print line.split()
-                header, sequence = line.split()
-                # current_output_text = ">" + line.replace("\t#\t", "\n")
-                print "header = %s" % header
-                print "sequence = %s" % sequence
-                # out_header = self.format_header(header)
+                out_header = self.format_header(line)
                 # print "out_header = %s" % out_header
                 # print "sequence = %s" % sequence
 
