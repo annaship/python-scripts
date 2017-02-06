701951cf2d14acf8022c7f533f981c419732cea8
annaship <tetya-loshad@yandex.ru>
2017-02-06 18:19:10 -0500

    clean_binomial

--- hlsu_tax.py
+++ hlsu_tax.py
@@ -11,7 +11,8 @@ class Taxonomy:
         self.output_text = {}
         self.ranks = ["domain", "phylum", "class", "order", "family", "genus", "species"]
         self.rank_level = ""
-        self.all_tax_ranks = collections.defaultdict()
+        # self.all_tax_ranks = collections.defaultdict()
+        self.all_tax_ranks = {}
         
     """
     time head -2 silva.all1.tax_fa | sed 's/^/>/' | sed 's/\t#\t/\n/' | tr "\t" ";" | awk 'BEGIN {FS=";"; OFS="\t"}  {if ($0 ~ /^>/) print $1, $(NF-1)"_" $NF, $(NF-1), "NA", $(NF-2); else print}'
@@ -59,7 +60,14 @@ class Taxonomy:
         except:
             raise
         
-    
+    def clean_binomial(self):
+        # print 'YYY: all_tax_ranks'
+        # print self.all_tax_ranks
+        #'JX660799': {'domain': 'Eukaryota', 'family': 'Phaeocystaceae', 'order': 'Phaeocystales', 'phylum': 'Haptophyta', 'species': 'Phaeocystis_sp._JD-2012', 'genus': 'Phaeocystis', 'class': 'Haptophyceae'}
+        for ref_id, tax_dict in self.all_tax_ranks.items():
+            if tax_dict["species"].startswith(tax_dict["genus"]):
+                tax_dict["species"] = tax_dict["species"].replace(tax_dict["genus"] + "_", "")
+
     def parse_line(self, line):
         # print header LSUcultures334_taxonomy.txt
         # AF289038	Eukaryota;Haptophyceae;Prymnesiales;Prymnesiaceae;Prymnesium;Prymnesium_parvum_f._patelliferum  1
@@ -68,9 +76,9 @@ class Taxonomy:
         ref_id, taxon_string, num = line.split()
         
         taxon_split = taxon_string.split(";")
-        if len(taxon_split) != 7:
-            print "len(taxon_split)"
-            print len(taxon_split) 
+        # if len(taxon_split) != 7:
+        #     print "len(taxon_split)"
+        #     print len(taxon_split)
 
         current_tax_dict = {}
         for idx, rank in enumerate(self.ranks):
@@ -111,12 +119,15 @@ class Taxonomy:
                 # current_output_text = ""
                 line = line.strip()
                 if not line: continue #Skip empty
-                out_header = self.parse_line(line)
+                # out_header =
+                self.parse_line(line)
                 
                 # print "out_header = %s" % out_header
                 # print "sequence = %s" % sequence
-        print 'YYY: all_tax_ranks'
-        print self.all_tax_ranks
+        # print 'YYY: all_tax_ranks'
+        # print self.all_tax_ranks
+        
+        
 
 
 if __name__ == '__main__':
@@ -137,4 +148,7 @@ if __name__ == '__main__':
 
     
     taxonomy.parse_taxonomy(args)
+    taxonomy.clean_binomial()
+    print 'YYY: all_tax_ranks'
+    print taxonomy.all_tax_ranks
     
