d75ef973810902de748c9f299d0a4ccfeeb5658d
annaship <tetya-loshad@yandex.ru>
2017-02-10 21:53:15 -0500

    Do you want to proceed?

--- fasta_two_lines.py
+++ fasta_two_lines.py
@@ -14,6 +14,39 @@ class My_fasta:
     self.args = args
     self.all_files = {}
     
+  def query_yes_no(self, question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+    
+    
   def get_files(self, walk_dir_name, ext = ""):
       # files = {}
       filenames = []
@@ -71,11 +104,17 @@ if __name__ == '__main__':
     help = """Output file name, default - input file name + '.unsplit.fa' or '.concat.fa' """)
   
   args = parser.parse_args()
-  if not len(sys.argv) > 1:
-      print """Running 'unsplit' with default parameters: input ext='.fa', start_dir='.', """
+  
 
           
   my_fasta = My_fasta(args)
+  if not len(sys.argv) > 1:
+      print """Running 'unsplit' with default parameters: input ext='.fa', start_dir='.', """
+      yes_no = my_fasta.query_yes_no("Do you want to proceed?")
+      # print "yes_no = "
+      # print yes_no
+      if not yes_no:
+          sys.exit()
   
   is_verbatim = args.is_verbatim
   
