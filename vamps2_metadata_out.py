8609b1bdc714756e9a28ab7dcd74aece8146738e
annaship <tetya-loshad@yandex.ru>
2017-04-10 16:56:47 -0400

    gets raw metadata

--- vamps2_metadata_out.py
+++ vamps2_metadata_out.py
@@ -18,30 +18,41 @@ class Metadata():
     project_ids = self.get_project_ids()
     utils.benchmark_w_return_2(t, "get_project_ids")
 
-    # t = utils.benchmark_w_return_1("all_project_ids_to_str")
-    # all_project_ids_str = self.all_project_ids_to_str(project_ids)
-    # utils.benchmark_w_return_2(t, "all_project_ids_to_str")
+    t = utils.benchmark_w_return_1("all_project_ids_to_str")
+    all_project_ids_str = self.all_project_ids_to_str(project_ids)
+    utils.benchmark_w_return_2(t, "all_project_ids_to_str")
     
     t = utils.benchmark_w_return_1("get_custom_fields")
-    custom_fields = self.get_custom_fields(self.all_project_ids_to_str(project_ids))
+    custom_fields = self.get_custom_fields(all_project_ids_str)
     utils.benchmark_w_return_2(t, "get_custom_fields")
 
+    t = utils.benchmark_w_return_1("make_custom_table_names_list")
+    custom_table_names_list = self.make_custom_table_names_list(all_project_ids_str)
+    utils.benchmark_w_return_2(t, "make_custom_table_names_list")
+
+
     t = utils.benchmark_w_return_1("get_project_datasets_metadata")
-    raw_metadata = self.get_project_datasets_metadata(custom_tables)
+    raw_metadata = self.get_project_datasets_metadata(custom_table_names_list)
     print raw_metadata
     utils.benchmark_w_return_2(t, "get_project_datasets_metadata")
         
-  def get_project_datasets_metadata(self, custom_tables):
+  def get_project_datasets_metadata(self, custom_table_names_list):
     custom_metadata_values_per_project_dataset = []
 
-    for custom_table in custom_tables:
+    for custom_table in custom_table_names_list:
       # TODO: combine with get_custom_metadata_per_project
-      poject_dataset_query = """SELECT DISTINCT project_id, project, dataset.dataset as sample, %s.* FROM %s JOIN dataset USING(dataset_id) JOIN project USING(project_id) JOIN required_metadata_info USING(dataset_id)"""
+      print "CCC"
+      print custom_table
+      poject_dataset_query = """SELECT DISTINCT project_id, project, dataset.dataset as sample, %s.*, required_metadata_info.* FROM %s JOIN dataset USING(dataset_id) JOIN project USING(project_id) JOIN required_metadata_info USING(dataset_id)""" % (custom_table, custom_table)
+      print "UUU"
+      print poject_dataset_query
       try:
-        res = mysql_utils.execute_fetch_select_where(poject_dataset_query, (custom_table, custom_table))
+        res = mysql_utils.execute_fetch_select_to_dict(poject_dataset_query)
+        # res = mysql_utils.execute_fetch_select_where(poject_dataset_query, (custom_table, custom_table))
         # print res
+        # {'adapter_sequence_id': 194, 'required_metadata_id': 16772L, 'geo_loc_name_id': 14975L, 'domain_id': 3L, 'dna_region_id': 13, 'updated_at': datetime.datetime(2017, 3, 13, 12, 54, 33), 'sample': 'Bering_4_Bv6v4', 'env_package_id': 22, 'sample_id': 'BS-4 (B)', 'dataset_id': 336382L, 'pH': '7.72', 'custom_metadata_432_id': 8L, 'required_metadata_info.dataset_id': 336382L, 'samp_store_temp': '-80', 'target_gene_id': 1, 'collection_date': '2009-08-07', 'samp_store_dur': 'P2Y', 'redox_state': 'anoxic', 'phosphate': '121.17', 'env_feature_id': 6191L, 'geo_loc_name': 'Bering Sea', 'latitude': 58.7, 'access_point_type': 'piston core', 'sequencing_platform_id': 1, 'project_id': 432L, 'chloride': '522290.7', 'dna_extraction_meth': 'Mo Bio Power Soil', 'diss_inorg_carb': '33000', 'rock_type': 'sediment', 'primer_suite_id': 9, 'index_sequence_id': 83, 'env_matter_id': 1281L, 'created_at': datetime.datetime(2016, 6, 17, 13, 42, 18), 'longitude': -178.56, 'project': 'DCO_WAL_Bv6v4', 'calcium': '2654.1', 'depth': 8.65, 'tot_depth_water_col': '1950.9', 'env_biome_id': 1116L})]
+        
         custom_metadata_values_per_project_dataset.append(res)
-        # (((300L, 'DCO_BKR_Av4v5', 'Knox_63E_6H2', 1L, 238918L, '2330', 'MP Biomedical FAST DNA', '451', '58.2', '8.02', 'Baltic Sea Basin', 'perfluorocarbon tracer', '70', '-80', '16S DNA', '8.82', 'Vared Clay/Silty Clay', '14.8', 'Knox63E6H2', 11.5), (300L, 'DCO_BKR_Av4v5', 'Aar_DrillFluid_59E_NC', 2L, 238915L, '', 'MP Biomedical FAST DNA', '', '', '', 'Baltic Sea Basin', '', '', '-80', '16S DNA', '', 'drill fluid', '', 'AarDrillFluid59ENC', 0.0), (300L, 'DCO_BKR_Av4v5', 'Aar_59E_25H2', 3L, 238914L, '810', 'MP Biomedical FAST DNA', '35', '6.1', '7.01', 'Baltic Sea Basin', 'perfluorocarbon tracer', '70', '-80', '16S DNA', '7.93', 'Vared Clay/Silty Clay', '9.05', 'Aar59E25H2', 81.5), (300L, 'DCO_BKR_Av4v5', 'Knox_65C_7H2', 4L, 238917L, '5400', 'MP Biomedical FAST DNA', '87', '7.17', '7.41', 'Baltic Sea Basin', 'perfluorocarbon tracer', '170', '-80', '16S DNA', '', 'Vared Clay/Silty Clay', '6.8', 'Knox65C7H2', 20.35), (300L, 'DCO_BKR_Av4v5', 'Knox_60B_10H2', 5L, 238916L, '0', 'MP Biomedical FAST DNA', '34', '9.05', '7.89', 'Baltic Sea Basin', 'perfluorocarbon tracer', '8300', '-80', '16S DNA', '8.36', 'Vared Clay/Silty Clay', '26.7', 'Knox60B10H2', 27.4)), ['project_id', 'project', 'sample', 'custom_metadata_300_id', 'dataset_id', 'methane', 'dna_extraction_meth', 'tot_depth_water_col', 'alkalinity', 'pH', 'geo_loc_name', 'quality_method', 'sulfate', 'samp_store_temp', 'target_gene', 'microbial_biomass_microscopic', 'lithology', 'salinity', 'sample_id', 'depth'])
       except MySQLdb.ProgrammingError:
         pass
       # _mysql_exceptions.ProgrammingError
@@ -89,6 +100,10 @@ class Metadata():
    
   def all_project_ids_to_str(self, all_project_ids):
     return [str(int(x[0])) for x in all_project_ids[0]]
+    
+  def make_custom_table_names_list(self, all_project_ids_str):
+    return ['custom_metadata_' + x for x in all_project_ids_str]
+    
 
 if __name__ == '__main__':
   utils = util.Utils()
