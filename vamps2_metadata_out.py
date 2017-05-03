94fee9b038e03fd111e543d3659caeacdfb635ef
annaship <tetya-loshad@yandex.ru>
2017-05-03 16:20:45 -0400

    make_raw_metadata_query

--- vamps2_metadata_out.py
+++ vamps2_metadata_out.py
@@ -319,127 +319,97 @@ class Metadata():
             raise
     return metadata_w_units_dict
 
+  def make_raw_metadata_query(self, addition_custom1, addition_custom2):
+    return """SELECT DISTINCT
+    `required_metadata_info`.`required_metadata_id` AS `required_metadata_id`,
+    `dataset`.`project_id` AS `project_id`,
+    `project`.`project` AS `project`,
+    `required_metadata_info`.`dataset_id` AS `dataset_id`,
+    `dataset`.`dataset` AS `dataset`,
+    `required_metadata_info`.`collection_date` AS `collection_date`,
+    `required_metadata_info`.`env_biome_id` AS `env_biome_id`,
+    `env_biome`.`term_name` AS `env_biome`,
+    `required_metadata_info`.`latitude` AS `latitude`,
+    `required_metadata_info`.`longitude` AS `longitude`,
+    `required_metadata_info`.`target_gene_id` AS `target_gene_id`,
+    `target_gene`.`target_gene` AS `target_gene`,
+    `required_metadata_info`.`dna_region_id` AS `dna_region_id`,
+    `dna_region`.`dna_region` AS `dna_region`,
+    `required_metadata_info`.`sequencing_platform_id` AS `sequencing_platform_id`,
+    `sequencing_platform`.`sequencing_platform` AS `sequencing_platform`,
+    `required_metadata_info`.`domain_id` AS `domain_id`,
+    `domain`.`domain` AS `domain`,
+    `required_metadata_info`.`geo_loc_name_id` AS `geo_loc_name_id`,
+    `geo_loc_name`.`term_name` AS `geo_loc_name`,
+    `required_metadata_info`.`env_feature_id` AS `env_feature_id`,
+    `env_feature`.`term_name` AS `env_feature`,
+    `required_metadata_info`.`env_matter_id` AS `env_matter_id`,
+    `env_matter`.`term_name` AS `env_matter`,
+    `required_metadata_info`.`env_package_id` AS `env_package_id`,
+    `env_package`.`env_package` AS `env_package`,
+    `required_metadata_info`.`adapter_sequence_id` AS `adapter_sequence_id`,
+    `run_key`.`run_key` AS `adapter_sequence`,
+    `required_metadata_info`.`illumina_index_id` AS `illumina_index_id`,
+    `illumina_index`.`illumina_index` AS `illumina_index`,
+    `run`.`run` AS `run`,
+    `required_metadata_info`.`primer_suite_id` AS `primer_suite_id`,
+    `primer_suite`.`primer_suite` AS `primer_suite`,
+    `required_metadata_info`.`created_at` AS `created_at`,
+    `required_metadata_info`.`updated_at` AS `updated_at`""" + addition_custom1 + """
+    FROM `required_metadata_info`
+    JOIN `dataset` USING(dataset_id)
+    JOIN `project` USING(project_id)
+    JOIN `dna_region` USING(dna_region_id)
+    JOIN `sequencing_platform` USING(sequencing_platform_id)
+    JOIN `target_gene` USING(target_gene_id)
+    JOIN `domain`  USING(domain_id)
+    JOIN `term` `env_feature` ON(`env_feature`.`term_id` = `required_metadata_info`.`env_feature_id`)
+    JOIN `term` `env_matter` ON(`env_matter`.`term_id` = `required_metadata_info`.`env_matter_id`)
+    JOIN `term` `env_biome` ON(`env_biome`.`term_id` = `required_metadata_info`.`env_biome_id`)
+    JOIN `term` `geo_loc_name` ON(`geo_loc_name`.`term_id` = `required_metadata_info`.`geo_loc_name_id`)
+    JOIN `env_package` USING(env_package_id)
+    JOIN `run_key` ON(`run_key`.`run_key_id` = `required_metadata_info`.`adapter_sequence_id`)
+    JOIN `illumina_index` USING(illumina_index_id)
+    JOIN `primer_suite` USING(primer_suite_id)
+    JOIN `run` USING(run_id)
+    """ + addition_custom2
+    
+    
   def get_raw_metadata(self, all_project_ids_str):
     raw_metadata = defaultdict(list)
+    
     for project_id_str in all_project_ids_str:
-      raw_metadata_query = """SELECT DISTINCT
-      `required_metadata_info`.`required_metadata_id` AS `required_metadata_id`,
-      `dataset`.`project_id` AS `project_id`,
-      `project`.`project` AS `project`,
-      `required_metadata_info`.`dataset_id` AS `dataset_id`,
-      `dataset`.`dataset` AS `dataset`,
-      `required_metadata_info`.`collection_date` AS `collection_date`,
-      `required_metadata_info`.`env_biome_id` AS `env_biome_id`,
-      `env_biome`.`term_name` AS `env_biome`,
-      `required_metadata_info`.`latitude` AS `latitude`,
-      `required_metadata_info`.`longitude` AS `longitude`,
-      `required_metadata_info`.`target_gene_id` AS `target_gene_id`,
-      `target_gene`.`target_gene` AS `target_gene`,
-      `required_metadata_info`.`dna_region_id` AS `dna_region_id`,
-      `dna_region`.`dna_region` AS `dna_region`,
-      `required_metadata_info`.`sequencing_platform_id` AS `sequencing_platform_id`,
-      `sequencing_platform`.`sequencing_platform` AS `sequencing_platform`,
-      `required_metadata_info`.`domain_id` AS `domain_id`,
-      `domain`.`domain` AS `domain`,
-      `required_metadata_info`.`geo_loc_name_id` AS `geo_loc_name_id`,
-      `geo_loc_name`.`term_name` AS `geo_loc_name`,
-      `required_metadata_info`.`env_feature_id` AS `env_feature_id`,
-      `env_feature`.`term_name` AS `env_feature`,
-      `required_metadata_info`.`env_matter_id` AS `env_matter_id`,
-      `env_matter`.`term_name` AS `env_matter`,
-      `required_metadata_info`.`env_package_id` AS `env_package_id`,
-      `env_package`.`env_package` AS `env_package`,
-      `required_metadata_info`.`adapter_sequence_id` AS `adapter_sequence_id`,
-      `run_key`.`run_key` AS `adapter_sequence`,
-      `required_metadata_info`.`illumina_index_id` AS `illumina_index_id`,
-      `illumina_index`.`illumina_index` AS `illumina_index`,
-      `run`.`run` AS `run`,
-      `required_metadata_info`.`primer_suite_id` AS `primer_suite_id`,
-      `primer_suite`.`primer_suite` AS `primer_suite`,
-      `required_metadata_info`.`created_at` AS `created_at`,
-      `required_metadata_info`.`updated_at` AS `updated_at`,
-      custom_metadata_%s.*
-      FROM `required_metadata_info`
-      JOIN `dataset` USING(dataset_id)
-      JOIN `project` USING(project_id)
-      JOIN `dna_region` USING(dna_region_id)
-      JOIN `sequencing_platform` USING(sequencing_platform_id)
-      JOIN `target_gene` USING(target_gene_id)
-      JOIN `domain`  USING(domain_id)
-      JOIN `term` `env_feature` ON(`env_feature`.`term_id` = `required_metadata_info`.`env_feature_id`)
-      JOIN `term` `env_matter` ON(`env_matter`.`term_id` = `required_metadata_info`.`env_matter_id`)
-      JOIN `term` `env_biome` ON(`env_biome`.`term_id` = `required_metadata_info`.`env_biome_id`)
-      JOIN `term` `geo_loc_name` ON(`geo_loc_name`.`term_id` = `required_metadata_info`.`geo_loc_name_id`)
-      JOIN `env_package` USING(env_package_id)
-      JOIN `run_key` ON(`run_key`.`run_key_id` = `required_metadata_info`.`adapter_sequence_id`)
-      JOIN `illumina_index` USING(illumina_index_id)
-      JOIN `primer_suite` USING(primer_suite_id)
-      JOIN `run` USING(run_id)
-      JOIN custom_metadata_%s USING(dataset_id);
-      """ % (project_id_str, project_id_str)
-
       try:
+        
+
+        addition_custom1 = """,
+        custom_metadata_%s.*"""
+
+        addition_custom2 = """JOIN custom_metadata_%s USING(dataset_id)"""
+        
+        raw_metadata_query_base = self.make_raw_metadata_query(addition_custom1, addition_custom2)
+        raw_metadata_query = raw_metadata_query_base % (project_id_str, project_id_str)
+        
+        print "RRR1"
+        print raw_metadata_query
+        
         res = mysql_utils.execute_fetch_select_to_dict(raw_metadata_query)
         raw_metadata[project_id_str] = res
       except MySQLdb.ProgrammingError:
         # pass
         # _mysql_exceptions.ProgrammingError
-        raw_metadata_query = """SELECT DISTINCT
-        `required_metadata_info`.`required_metadata_id` AS `required_metadata_id`,
-        `dataset`.`project_id` AS `project_id`,
-        `project`.`project` AS `project`,
-        `required_metadata_info`.`dataset_id` AS `dataset_id`,
-        `dataset`.`dataset` AS `dataset`,
-        `required_metadata_info`.`collection_date` AS `collection_date`,
-        `required_metadata_info`.`env_biome_id` AS `env_biome_id`,
-        `env_biome`.`term_name` AS `env_biome`,
-        `required_metadata_info`.`latitude` AS `latitude`,
-        `required_metadata_info`.`longitude` AS `longitude`,
-        `required_metadata_info`.`target_gene_id` AS `target_gene_id`,
-        `target_gene`.`target_gene` AS `target_gene`,
-        `required_metadata_info`.`dna_region_id` AS `dna_region_id`,
-        `dna_region`.`dna_region` AS `dna_region`,
-        `required_metadata_info`.`sequencing_platform_id` AS `sequencing_platform_id`,
-        `sequencing_platform`.`sequencing_platform` AS `sequencing_platform`,
-        `required_metadata_info`.`domain_id` AS `domain_id`,
-        `domain`.`domain` AS `domain`,
-        `required_metadata_info`.`geo_loc_name_id` AS `geo_loc_name_id`,
-        `geo_loc_name`.`term_name` AS `geo_loc_name`,
-        `required_metadata_info`.`env_feature_id` AS `env_feature_id`,
-        `env_feature`.`term_name` AS `env_feature`,
-        `required_metadata_info`.`env_matter_id` AS `env_matter_id`,
-        `env_matter`.`term_name` AS `env_matter`,
-        `required_metadata_info`.`env_package_id` AS `env_package_id`,
-        `env_package`.`env_package` AS `env_package`,
-        `required_metadata_info`.`adapter_sequence_id` AS `adapter_sequence_id`,
-        `run_key`.`run_key` AS `adapter_sequence`,
-        `required_metadata_info`.`illumina_index_id` AS `illumina_index_id`,
-        `illumina_index`.`illumina_index` AS `illumina_index`,
-        `run`.`run` AS `run`,
-        `required_metadata_info`.`primer_suite_id` AS `primer_suite_id`,
-        `primer_suite`.`primer_suite` AS `primer_suite`,
-        `required_metadata_info`.`created_at` AS `created_at`,
-        `required_metadata_info`.`updated_at` AS `updated_at`
-        FROM `required_metadata_info`
-        JOIN `dataset` USING(dataset_id)
-        JOIN `project` USING(project_id)
-        JOIN `dna_region` USING(dna_region_id)
-        JOIN `sequencing_platform` USING(sequencing_platform_id)
-        JOIN `target_gene` USING(target_gene_id)
-        JOIN `domain`  USING(domain_id)
-        JOIN `term` `env_feature` ON(`env_feature`.`term_id` = `required_metadata_info`.`env_feature_id`)
-        JOIN `term` `env_matter` ON(`env_matter`.`term_id` = `required_metadata_info`.`env_matter_id`)
-        JOIN `term` `env_biome` ON(`env_biome`.`term_id` = `required_metadata_info`.`env_biome_id`)
-        JOIN `term` `geo_loc_name` ON(`geo_loc_name`.`term_id` = `required_metadata_info`.`geo_loc_name_id`)
-        JOIN `env_package` USING(env_package_id)
-        JOIN `run_key` ON(`run_key`.`run_key_id` = `required_metadata_info`.`adapter_sequence_id`)
-        JOIN `illumina_index` USING(illumina_index_id)
-        JOIN `primer_suite` USING(primer_suite_id)
-        JOIN `run` USING(run_id)
+        
+        addition_custom1 = ""
+        addition_custom2 = """
         WHERE project_id = %s
         ;
-        """ % (project_id_str)
-        print "FFF"
+        """ 
+        
+        raw_metadata_query_base = self.make_raw_metadata_query(addition_custom1, addition_custom2)
+        
+        raw_metadata_query = raw_metadata_query_base % (project_id_str)
+        
+        print "RRR2"
         print raw_metadata_query
         res = mysql_utils.execute_fetch_select_to_dict(raw_metadata_query)
         raw_metadata[project_id_str] = res
