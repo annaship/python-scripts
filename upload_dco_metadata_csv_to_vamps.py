ca3823aa10a85dd91cfffabfe0aff6104c6de72f
annaship <tetya-loshad@yandex.ru>
2017-07-28 17:52:09 -0400

    get req intersection

--- upload_dco_metadata_csv_to_vamps.py
+++ upload_dco_metadata_csv_to_vamps.py
@@ -16,17 +16,18 @@ class Metadata():
   def __init__(self):
     csv_file_fields, csv_file_content = self.get_data_from_csv()
     self.get_required_fields(csv_file_fields)
-    
-  def get_data_from_csv(self):
-    # TODO: get from args
-    file_name = "/Users/ashipunova/Downloads/metadata-project_DCO_GAI_Bv3v5_AnnaSh_1501274966258.csv"
-    return utils.read_csv_into_list(file_name)
     # print "csv_file_fields = "
     # print csv_file_fields
     #
     #
     # print "csv_file_content = "
     # print csv_file_content
+    
+  def get_data_from_csv(self):
+    # TODO: get from args
+    file_name = "/Users/ashipunova/Downloads/metadata-project_DCO_GAI_Bv3v5_AnnaSh_1501274966258.csv"
+    return utils.read_csv_into_list(file_name)
+
     
   def get_field_names(self, table_name):
     query = """
@@ -41,11 +42,29 @@ class Metadata():
   def get_required_fields(self, csv_file_fields):
     #required_metadata_info
     # pass
-    field_names = self.get_field_names("required_metadata_info")
-    # print field_names
+    req_field_names_t = self.get_field_names("required_metadata_info")
+    print req_field_names_t
     
-    for field in csv_file_fields:
-      print field
+    # for tup in req_field_names[0]:
+      
+      
+    #   print field
+    # print "type(req_field_names)"
+    # print type(req_field_names)
+# <type 'tuple'>
+    # print list(req_field_names)
+    
+    req_field_names = zip(*req_field_names_t[0])
+    print req_field_names
+
+    print "type(csv_file_fields)"
+    print type(csv_file_fields)
+# <type 'list'>
+
+    intersection = list(set(req_field_names[0]) & set(csv_file_fields))
+    print "\nintersection == "
+    print intersection
+    # ['collection_date', 'latitude', 'dataset_id', 'longitude']
     
     
   def get_custom_field_names(self):
