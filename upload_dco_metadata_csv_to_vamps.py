34d626c0f481df0eab86b3384765b68f51d75ad1
annaship <tetya-loshad@yandex.ru>
2017-07-28 17:19:01 -0400

    get_required_fields_names

--- upload_dco_metadata_csv_to_vamps.py
+++ upload_dco_metadata_csv_to_vamps.py
@@ -9,22 +9,39 @@ from collections import defaultdict
 
 class Metadata():
   # parse csv
+  # separate required from custom
+  # find ids by value
+  # find and print errors
+  
   def __init__(self):
     self.get_data_from_csv()
+    self.get_required_fields_names()
     
   def get_data_from_csv(self):
     # TODO: get from args
     file_name = "/Users/ashipunova/Downloads/metadata-project_DCO_GAI_Bv3v5_AnnaSh_1501274966258.csv"
     csv_file_fields, csv_file_content = utils.read_csv_into_list(file_name)
     print "csv_file_fields = "
-    print csv_file_fields
+    # print csv_file_fields
     
-    print "csv_file_content"
-    print csv_file_content
+    print "csv_file_content = "
+    # print csv_file_content
     
   def get_required_fields_names(self):
     #required_metadata_info
-    pass
+    # pass
+    table_name = "required_metadata_info"
+    query = """
+      SELECT COLUMN_NAME 
+      FROM INFORMATION_SCHEMA.COLUMNS 
+      WHERE TABLE_SCHEMA='vamps2' 
+          AND TABLE_NAME='%s'; 
+    """ % table_name
+    print query
+    
+    field_names = mysql_utils.execute_fetch_select(query)
+    print field_names
+    
     
   def get_custom_field_names(self):
     pass
