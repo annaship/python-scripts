cd04a5983b0faee4dceb14a0d7c43469fefe9736
annaship <tetya-loshad@yandex.ru>
2018-03-06 09:41:32 -0500

    rm grouper

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import logging
 import timeit
 import time
 import csv
-from itertools import izip_longest
+# from itertools import izip_longest
 
 class Mysql_util:
     """
@@ -349,9 +349,9 @@ class Utils:
     def initialize_dict_of_lists(self, list_of_keys):
       return {key: [] for key in list_of_keys}
 
-    def grouper(self, iterable, n, fillvalue=None):
-        args = [iter(iterable)] * n
-        return izip_longest(*args, fillvalue=fillvalue)
+    # def grouper(self, iterable, n, fillvalue=None):
+    #     args = [iter(iterable)] * n
+    #     return izip_longest(*args, fillvalue=fillvalue)
 
     """
     >>> from collections import defaultdict
