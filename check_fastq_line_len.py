b4c2e8727b7bfde3d128bc2dab39f725399533a7
annaship <tetya-loshad@yandex.ru>
2018-03-15 14:53:27 -0400

    to py3

--- check_fastq_line_len.py
+++ check_fastq_line_len.py
@@ -22,27 +22,27 @@ def check_if_verb():
   except IndexError:
     return False
   except: 
-    print "Unexpected error:", sys.exc_info()[0]
+    print("Unexpected error:", sys.exc_info()[0])
     return False
   return False
 
 
 start_dir = sys.argv[1]
-print "Start from %s" % start_dir
-print "Getting file names"
+print("Start from %s" % start_dir)
+print("Getting file names")
 
 all_dirs = set()
 
 #fq_files = get_files("/xraid2-2/sequencing/Illumina", ".fastq.gz")
 # "/xraid2-2/sequencing/Illumina/20151014ns"
 fq_files = get_files(start_dir, ".fastq.gz")
-print "Found %s fastq.gz files" % (len(fq_files))
+print("Found %s fastq.gz files" % (len(fq_files)))
 
 check_if_verb = check_if_verb()
 
 for file_name in fq_files:
   if (check_if_verb):
-    print file_name
+    print(file_name)
 
   try:
     f_input  = fq.FastQSource(file_name, True)
@@ -52,15 +52,15 @@ for file_name in fq_files:
       e = f_input.entry
       seq_len = len(e.sequence)
       qual_scores_len = len(e.qual_scores)
-      #print e.header_line
+      #print(e.header_line)
       if (seq_len != qual_scores_len):
-        print "WARNING, sequence and qual_scores_line have different length in %s" % file_name
+        print("WARNING, sequence and qual_scores_line have different length in %s" % file_name)
         all_dirs.add(fq_files[file_name][0])
   except RuntimeError:
     if (check_if_verb):
-      print sys.exc_info()[0]
+      print(sys.exc_info()[0])
   except:
-    print "Unexpected error:", sys.exc_info()[0]
+    print("Unexpected error:", sys.exc_info()[0])
     next
 
 print all_dirs
