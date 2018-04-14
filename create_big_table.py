ea32f2c0c74014f401e197c74bbe5ce072c856e3
annaship <tetya-loshad@yandex.ru>
2018-04-14 12:22:31 -0400

    alter

--- create_big_table.py
+++ create_big_table.py
@@ -17,10 +17,26 @@ class Query():
   def __init__(self):
     self.create_table_look_up_tax_query = """CREATE TABLE IF NOT EXISTS look_up_tax
       AS
-      SELECT seq_count, dataset_id, domain_id, phylum_id, klass_id, order_id, family_id, genus_id, species_id, strain_id, sequence_id FROM sequence_pdr_info JOIN silva_taxonomy_info_per_seq USING(sequence_id) JOIN silva_taxonomy USING(silva_taxonomy_id) limit 1"""
+      SELECT seq_count, dataset_id, domain_id, phylum_id, klass_id, order_id, family_id, genus_id, species_id, strain_id, sequence_id, run_info_ill_id FROM sequence_pdr_info JOIN silva_taxonomy_info_per_seq USING(sequence_id) JOIN silva_taxonomy USING(silva_taxonomy_id) limit 1"""
   
-  def create_table_look_up_tax(self):
+  def alter_table_look_up_tax(self):
+    q = """ALTER TABLE look_up_tax
+    ADD COLUMN look_up_tax_id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY FIRST,
+ADD UNIQUE KEY seq_dat (sequence_id, run_info_ill_id),
+ADD KEY all_fields (dataset_id, domain_id, phylum_id, klass_id, order_id, family_id, genus_id, species_id, strain_id);"""
+    try:
+      res = mysql_utils.execute_no_fetch_w_info(q)
+      print(q, res)
+    except MySQLdb.OperationalError as e:
+      print(e)
+      pass
+
+
+  def update_table_look_up_tax(self):
     pass
+
+
+
 
 if __name__ == '__main__':
   utils = util.Utils()
@@ -33,8 +49,12 @@ if __name__ == '__main__':
   # project_name_startswith = sys.argv[1] if len(sys.argv) == 2 else 'DCO'
   q = Query()
   res = mysql_utils.execute_no_fetch_w_info(q.create_table_look_up_tax_query)
-  print(res)
+  print(q.create_table_look_up_tax_query, res)
+
+  q.alter_table_look_up_tax()
   
+  """TODO: args - create, update"""
+
   #
   # t = utils.benchmark_w_return_1("get_metadata_info")
   # metadata.get_metadata_info()
