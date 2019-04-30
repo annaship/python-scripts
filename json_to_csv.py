521c388d4afa6adcfce28eff1b92b6bf1c2fc56d
annaship <tetya-loshad@yandex.ru>
2019-04-29 21:15:12 -0400

    flatten

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,43 +1,40 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
 import json
 import csv
 
-# def main():
-
-def parse_json(line):
-  line1 = line.lstrip("[")
-  x = line1.replace("]", "")
-  data_parsed = json.loads(x)
-  # print(data_parsed)
-  # w.writelines(bunch)
-  header = data_parsed.keys()
-  # print(header)
-  return (header, data_parsed)
-
-  # for i in range(0,length_data)
-      # meetup = data_parsed[i].values()
-      # csv_writer.writerow([meetup])
-  
-
-def ProcessLargeTextFile():
-  file_in = "test2.json"
-  file_out = "test2_out.json"
-  with open(file_in) as infile, open(file_out, "w") as out_file:
-    csv_writer = csv.writer(out_file)
-    header = ''
-    for line in infile:
-      curr_header, data_dict = parse_json(line)
-      if curr_header != header:
-        csv_writer.writerow(curr_header)
-        header = curr_header
-
-
-
-  # header = data_parsed[0].keys()
-  # csv_writer.writerow(header)
-  #
-  # for i in range(0,length_data)
-  #     meetup = data_parsed[i].values()
-  #     csv_writer.writerow([meetup])
+def get_leaves(item, key=None):
+    if isinstance(item, dict):
+        leaves = []
+        for i in item.keys():
+            leaves.extend(get_leaves(item[i], i))
+        return leaves
+    elif isinstance(item, list):
+        leaves = []
+        for i in item:
+            leaves.extend(get_leaves(i, key))
+        return leaves
+    else:
+        return [(key, item)]
 
 if __name__ == "__main__":
-    ProcessLargeTextFile()
+  file_in = "test2.json"
+  file_out = "test2_out.json"
+  with open(file_in) as f_input, open(file_out, "wb") as f_output:
+      csv_output = csv.writer(f_output)
+      write_header = True
+
+      for entry in json.load(f_input):
+          leaf_entries = sorted(get_leaves(entry))
+
+          if write_header:
+              csv_output.writerow([k for k, v in leaf_entries])
+              write_header = False
+
+          # self.writer.writerow([unicode(s).encode("utf-8") for s in row])
+
+          csv_output.writerow([unicode(v).encode("utf-8") for k, v in leaf_entries])
+  #
+# if __name__ == "__main__":
+#     ProcessLargeTextFile()
