750de2eadc5fe1095b8c8bed94f87bd8a9aac61a
annaship <tetya-loshad@yandex.ru>
2019-04-29 22:19:25 -0400

    works

--- json_to_csv.py
+++ json_to_csv.py
@@ -28,18 +28,26 @@ def get_leaves(item, key=None, n=None):
 if __name__ == "__main__":
   file_in = "test2.json"
   file_out = "test2_out.json"
-  with open(file_in) as f_input, io.open(file_out, "w", encoding='utf8') as f_output:
-      csv_output = csv.writer(f_output, delimiter=";")
+  # , encoding='utf-8'
+  with open(file_in) as f_input, io.open(file_out, "wb") as f_output:
+      csv_output = csv.writer(f_output, delimiter=";", quoting=csv.QUOTE_ALL)
       write_header = True
 
       for entry in json.load(f_input):
           leaf_entries = sorted(get_leaves(entry))
           print(leaf_entries)
           if write_header:
-              csv_output.writerow([k for k, v in leaf_entries])
+              row = [k for k, v in leaf_entries]
+              csv_output.writerow(row)
               write_header = False
 
           csv_output.writerow([unicode(v).encode("utf-8") for k, v in leaf_entries])
   #
 # if __name__ == "__main__":
 #     ProcessLargeTextFile()
+# with io.open("testJson.json",'w',encoding="utf-8") as outfile:
+  # outfile.write(unicode(json.dumps(cards, ensure_ascii=False)))
+# with open('output_file_name', 'w', newline='', encoding='utf-8') as csv_file:
+    # writer = csv.writer(csv_file, delimiter=';')
+    # writer.writerow('my_utf8_string')
+
