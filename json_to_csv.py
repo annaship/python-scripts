351496fc1a523b5e47fbe586817b24f1df64055d
annaship <tetya-loshad@yandex.ru>
2019-04-29 21:41:38 -0400

    count levels

--- json_to_csv.py
+++ json_to_csv.py
@@ -5,13 +5,16 @@ import json
 import csv
 import io
 
-def get_leaves(item, key=None):
+def get_leaves(item, key=None, n=None):
+    if n == None:
+        n = 0
     if isinstance(item, dict):
+        n = n + 1
         msg = "=" * 3 + "dict" + "=" * 3 
         print(msg)
         leaves = []
         for i in item.keys():
-            leaves.extend(get_leaves(item[i], i))
+            leaves.extend(get_leaves(item[i], i, n))
             print i
         return leaves
     elif isinstance(item, list):
@@ -22,7 +25,33 @@ def get_leaves(item, key=None):
             leaves.extend(get_leaves(i, key))
         return leaves
     else:
+        print("n = %d") % n
         return [(key, item)]
+        
+#Function that recursively extracts values out of the object into a flattened dictionary
+def flatten_json(data):
+    flat = [] #list of flat dictionaries
+    def flatten(y):
+        out = {}
+
+        def flatten2(x, name=''):
+            if type(x) is dict:
+                for a in x:
+                    print("a = %s") % a
+                    if a == "name": 
+                            flatten2(x["value"], name + x[a] + '_')
+                    else:  
+                        flatten2(x[a], name + a + '_')
+            elif type(x) is list:
+                for a in x:
+                    flatten2(a, name + '_')
+            else:
+                out[name[:-1]] = x
+
+        flatten2(y)
+        print("out")
+        print(out)
+        return out
 
 if __name__ == "__main__":
   file_in = "test2.json"
