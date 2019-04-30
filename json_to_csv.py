a90e801c93718cf029054a082cbc59c7a6e56a12
annaship <tetya-loshad@yandex.ru>
2019-04-29 20:35:22 -0400

    json_to_csv.py

---
+++ json_to_csv.py
@@ -0,0 +1,36 @@
+import json
+import csv
+
+# def main():
+
+def do_something_with(line):
+  line1 = line.lstrip("[")
+  y = line1.replace("]", "")
+  print(y)
+
+def ProcessLargeTextFile():
+  with open("test2.json") as infile:
+    for line in infile:
+      do_something_with(line)
+
+  
+  # some JSON:
+  # x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+  # parse x:
+  # y = json.loads(x)
+
+  # the result is a Python dictionary:
+  # print(y["age"])
+
+  # data_parsed = json.loads(Data)
+
+  # header = data_parsed[0].keys()
+  # csv_writer.writerow(header)
+  #
+  # for i in range(0,length_data)
+  #     meetup = data_parsed[i].values()
+  #     csv_writer.writerow([meetup])
+
+if __name__ == "__main__":
+    ProcessLargeTextFile()
