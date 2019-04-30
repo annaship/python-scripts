c1a27d3ad5f88ecdd014e1c0854511734354aa97
annaship <tetya-loshad@yandex.ru>
2019-04-30 12:28:32 -0400

    doesn't work

--- json_to_csv_all.py
+++ json_to_csv_all.py
@@ -25,15 +25,18 @@ def get_leaves(item, key=None, n=None):
     else:
         return [(key, item)]
 
-def split_json(f_input):
+def split_str(f_input):
   all_data1 = f_input.read()
-  rep = '},%s{' % (os.linesep)
+  all_data1_ = all_data1.replace('[', '')
+  all_data1 = all_data1.replace(']', '')
+  rep = '},###%s{' % (os.linesep)
   all_data_sep = all_data1.replace('},{', rep)
-  return all_data_sep
+  all_data_sep_list = all_data_sep.split("###")
+  return all_data_sep_list
   
-  with open(filepath, 'w') as f:
-      for chunk in json.JSONEncoder().iterencode(object_to_encode):
-          f.write(chunk)
+  # with open(filepath, 'w') as f:
+  #     for chunk in json.JSONEncoder().iterencode(object_to_encode):
+  #         f.write(chunk)
 
 if __name__ == "__main__":
   file_in = "test.json"
@@ -42,17 +45,27 @@ if __name__ == "__main__":
       csv_output = csv.writer(f_output, delimiter=";", quoting=csv.QUOTE_ALL)
       write_header = True
       
-      all_data_sep = split_json(f_input)
+      all_data_sep_list = split_str(f_input)
       print("==" * 3)
-      print(all_data_sep)
+      print(type(all_data_sep_list)) # str
       #json.loads(json.dumps([dict1, dict2]))
       # [{}, {}]
       # all_data_sep_list = json.dumps([all_data_sep])
           # for entry in json.loads(all_data_sep):
-      for chunk in json.JSONEncoder().iterencode(all_data_sep):
-          print("*" * 3)
+      # for chunk in json.JSONEncoder().iterencode(all_data_sep):
+      for chunk in all_data_sep_list:
+          entry = ""
+          print("chunk: " + "=" * 3)
           print(chunk)
-          entry = json.JSONDecoder().decode(chunk)
+          try:
+            # entry = json.JSONDecoder().decode(chunk)
+            entry = json.loads(chunk)
+            # entry = json.JSONEncoder().encode(chunk)
+          except ValueError:
+            print("ERR: " + "*" * 3)
+            print(chunk)
+            raise
+            
           leaf_entries = sorted(get_leaves(entry))
           if write_header:
               row = [k for k, v in leaf_entries]
