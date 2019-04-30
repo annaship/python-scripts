8d7414813c9c117e1759fe8ec7b145b3044d1ea1
annaship <tetya-loshad@yandex.ru>
2019-04-30 12:57:14 -0400

    add benchmark

--- json_to_csv_all.py
+++ json_to_csv_all.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
+import time
 import os
 import json
 import csv
@@ -32,14 +33,22 @@ def split_str(f_input):
   all_data_sep_list = all_data_sep.split("###")
   return all_data_sep_list
 
+def elapsed(start_name):
+    return time.time() - start_name
+      
 if __name__ == "__main__":
+  start_all = time.time()
+  
   file_in = "test.json"
   file_out = "test_out.json"
   with open(file_in) as f_input, open(file_out, "wt") as f_output:
       csv_output = csv.writer(f_output, delimiter=";", quoting=csv.QUOTE_ALL)
       write_header = True
 
+      start_sep = time.time()
       all_data_sep_list = split_str(f_input)
+      time_sep = elapsed(start_sep)
+      
       for chunk in all_data_sep_list:
           entry = ""
           try:
@@ -49,10 +58,30 @@ if __name__ == "__main__":
             print(chunk)
             raise
 
+          time_convert = time.time()
           leaf_entries = sorted(get_leaves(entry))
+          time_convert = elapsed(start_all)
+          
+          time_write_csv = time.time()
           if write_header:
               row = [k for k, v in leaf_entries]
               csv_output.writerow(row)
               write_header = False
 
           csv_output.writerow([v for k, v in leaf_entries])
+          time_write_csv = elapsed(start_all)
+          
+          
+  time_all = elapsed(start_all)
+  print("time_all in sec = ")
+  print(time_all)
+
+  print("time_sep in sec = ")
+  print(time_sep)
+  
+  print("time_convert in sec = ")
+  print(time_convert)
+  
+  print("time_write_csv in sec = ")
+  print(time_write_csv)
+  
