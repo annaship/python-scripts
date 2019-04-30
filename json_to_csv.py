ba858cb00bd28a481508f63e5603270a142cd116
annaship <tetya-loshad@yandex.ru>
2019-04-29 21:30:50 -0400

    TypeError: must be unicode, not str

--- json_to_csv.py
+++ json_to_csv.py
@@ -3,14 +3,20 @@
 
 import json
 import csv
+import io
 
 def get_leaves(item, key=None):
     if isinstance(item, dict):
+        msg = "=" * 3 + "dict" + "=" * 3 
+        print(msg)
         leaves = []
         for i in item.keys():
             leaves.extend(get_leaves(item[i], i))
+            print i
         return leaves
     elif isinstance(item, list):
+        msg = "-" * 3 + "list" + "-" * 3 
+        print(msg)
         leaves = []
         for i in item:
             leaves.extend(get_leaves(i, key))
@@ -21,18 +27,16 @@ def get_leaves(item, key=None):
 if __name__ == "__main__":
   file_in = "test2.json"
   file_out = "test2_out.json"
-  with open(file_in) as f_input, open(file_out, "wb") as f_output:
-      csv_output = csv.writer(f_output)
+  with open(file_in) as f_input, io.open(file_out, "w", encoding='utf8') as f_output:
+      csv_output = csv.writer(f_output, delimiter=";")
       write_header = True
 
       for entry in json.load(f_input):
           leaf_entries = sorted(get_leaves(entry))
 
           if write_header:
-              csv_output.writerow([k for k, v in leaf_entries])
+              csv_output.writerow([unicode(k).encode("utf-8") for k, v in leaf_entries])
               write_header = False
-
-          # self.writer.writerow([unicode(s).encode("utf-8") for s in row])
 
           csv_output.writerow([unicode(v).encode("utf-8") for k, v in leaf_entries])
   #
