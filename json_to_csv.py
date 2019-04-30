e43c6870d1527fe8455aae2ae37b5e30d62c4cae
annaship <tetya-loshad@yandex.ru>
2019-04-29 22:04:56 -0400

    correct keys

--- json_to_csv.py
+++ json_to_csv.py
@@ -17,60 +17,79 @@ def flatten(entry, n=None):
             print("!!!")
             print(new_key)
     
-
 def get_leaves(item, key=None, n=None):
     if n == None:
         n = 0
     if isinstance(item, dict):
         n = n + 1
-        msg = "=" * 3 + "dict" + "=" * 3 
-        # print(msg)
-        # print(item)
         leaves = []
         for i in item.keys():
             new_key = i
             if n == 2:
                 new_key = "nomenclature.%s" % i
             leaves.extend(get_leaves(item[i], new_key, n))
-            # print i
         return leaves
     elif isinstance(item, list):
-        msg = "-" * 3 + "list" + "-" * 3 
-        # print(msg)
         leaves = []
         for i in item:
             leaves.extend(get_leaves(i, key))
         return leaves
     else:
-        # print("n = %d") % n
-        # print("key = %s, item = %s") % (key, item)
         return [(key, item)]
         
+# def get_leaves(item, key=None, n=None):
+#     if n == None:
+#         n = 0
+#     if isinstance(item, dict):
+#         n = n + 1
+#         msg = "=" * 3 + "dict" + "=" * 3
+#         # print(msg)
+#         # print(item)
+#         leaves = []
+#         for i in item.keys():
+#             new_key = i
+#             if n == 2:
+#                 new_key = "nomenclature.%s" % i
+#             leaves.extend(get_leaves(item[i], new_key, n))
+#             # print i
+#         return leaves
+#     elif isinstance(item, list):
+#         msg = "-" * 3 + "list" + "-" * 3
+#         # print(msg)
+#         leaves = []
+#         for i in item:
+#             leaves.extend(get_leaves(i, key))
+#         return leaves
+#     else:
+#         # print("n = %d") % n
+#         # print("key = %s, item = %s") % (key, item)
+#         return [(key, item)]
+#
 #Function that recursively extracts values out of the object into a flattened dictionary
-def flatten_json(data):
-    flat = [] #list of flat dictionaries
-    def flatten(y):
-        out = {}
-
-        def flatten2(x, name=''):
-            if type(x) is dict:
-                for a in x:
-                    print("a = %s") % a
-                    if a == "name": 
-                            flatten2(x["value"], name + x[a] + '_')
-                    else:  
-                        flatten2(x[a], name + a + '_')
-            elif type(x) is list:
-                for a in x:
-                    flatten2(a, name + '_')
-            else:
-                out[name[:-1]] = x
-
-        flatten2(y)
-        # print("out")
-        # print(out)
-        return out
-
+# def flatten_json(data):
+#     flat = [] #list of flat dictionaries
+#     def flatten(y):
+#         out = {}
+#
+#         def flatten2(x, name=''):
+#             if type(x) is dict:
+#                 for a in x:
+#                     print("a = %s") % a
+#                     if a == "name":
+#                             flatten2(x["value"], name + x[a] + '_')
+#                     else:
+#                         flatten2(x[a], name + a + '_')
+#             elif type(x) is list:
+#                 for a in x:
+#                     flatten2(a, name + '_')
+#             else:
+#                 out[name[:-1]] = x
+#
+#         flatten2(y)
+#         # print("out")
+#         # print(out)
+#         return out
+#
 if __name__ == "__main__":
   file_in = "test2.json"
   file_out = "test2_out.json"
@@ -82,9 +101,9 @@ if __name__ == "__main__":
           # print(entry)
           # leaf_entries = flatten(entry)
           leaf_entries = sorted(get_leaves(entry))
-
+          print(leaf_entries)
           if write_header:
-              csv_output.writerow([unicode(k).encode("utf-8") for k, v in leaf_entries])
+              csv_output.writerow([k for k, v in leaf_entries])
               write_header = False
 
           csv_output.writerow([unicode(v).encode("utf-8") for k, v in leaf_entries])
