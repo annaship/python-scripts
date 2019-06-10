c79f92a7f620c1192f1ee42e758165509664b185
annaship <tetya-loshad@yandex.ru>
2019-06-10 19:58:25 -0400

    Update cnt_total.py

--- cnt_total.py
+++ cnt_total.py
@@ -3,53 +3,46 @@ import pprint
 from collections import defaultdict
 pp = pprint.PrettyPrinter(indent=4)
 
-args = sys.argv
-filename = args[1]
-fh = open(filename,'r')
-all_lines = fh.readlines()
-fh.close()
+def convert_to_num(str_cnt):
+    tmp_cnt = str_cnt.rstrip("ms\n")
+    # pp.pprint(tmp_cnt)
+  
+    num_cnt = float(tmp_cnt)
+    # pp.pprint(num_cnt)  
+    return num_cnt
+
+def get_file_content():
+    args = sys.argv
+    filename = args[1]
+    fh = open(filename,'r')
+    all_lines = fh.readlines()
+    fh.close()
+    return all_lines
 
 all_totals = defaultdict()
+all_lines = get_file_content()
+
 for line in all_lines:
   try:
     line_arr = line.split(": ")
     name = line_arr[1]
     str_cnt = line_arr[2]
 
-    tmp_cnt = str_cnt.rstrip("ms\n")
-    # pp.pprint(tmp_cnt)
-  
-    num_cnt = float(tmp_cnt)
-    # pp.pprint(num_cnt)
+    num_cnt = convert_to_num(str_cnt)
 
     curr_cnt = 0
     try:
       curr_cnt = all_totals[name]
-      # pp.pprint(curr_cnt)
     except KeyError:
       all_totals[name] = 0
       
     all_totals[name] = curr_cnt + num_cnt
-
-    # all_totals[name] += 
-  # print("LINE: %s" % line)
+    
   except:
     raise
 
 print("all_totals: ")
-pp.pprint(all_totals)
+for k, v in all_totals.items():
+  print("%s: %.2f" % (k, v)) 
+# pp.pprint(all_totals)
 
-def convert_to_num(cnt):
-  tmp_cnt = cnt.rstrip("ms")
-  pp.pprint(tmp_cnt)
-  
-  res_cnt = float(tmp_cnt)
-  pp.pprint(res_cnt)
-  
-  return res_cnt
-  
-
-# for name, cnt in all_totals.items:
-  # print("%s total:\t\t%d" % (name, cnt))
-
-  
