4573fa8bffa9232fc99820c8677013c6c4656c29
annaship <tetya-loshad@yandex.ru>
2019-06-28 04:15:25 -0400

    clean up

--- reformat_sample_seq_freq.py
+++ reformat_sample_seq_freq.py
@@ -61,14 +61,6 @@ seq_tax_dict = get_seq_tax(seq_tax_file_name)
 
 file_names = ["test1.csv", "test2.csv"]
 
-# fp = open(full_name, 'r')
-# with open(filepath) as fp:
-#    for cnt, line in enumerate(fp):
-#        # print("Line {}: {}".format(cnt, line))
-
-# seq_tax = open(seq_tax_file_name, 'r')
-
-
 all_dict = defaultdict()
 # 
 
@@ -76,15 +68,12 @@ for file_name in file_names:
   full_name = os.path.join(my_dir, file_name)
   # print(full_name)
   content = get_file_content(full_name) 
-  # cnt = 1
   for line in content:
-     # cnt += 1
      
      line_arr = line.strip().split()
      # print("Line {}: {}".format(cnt, line_arr))
      # Line 11: ['BBO_IGM_Bv4v5__R3_2', '118510832', '1', 'Bacteria;Firmicutes;Bacilli;Bacillales;Bacillaceae;Bacillus']
 
-     # all_dict[line_arr[0]]
      pr_dat = line_arr[0]
      seq = line_arr[1]
      freq = line_arr[2]
