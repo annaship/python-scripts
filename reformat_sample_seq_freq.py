db344a7205ed4048b42fe81301d460a4b70e981c
annaship <tetya-loshad@yandex.ru>
2019-06-28 03:09:31 -0400

    get content

--- reformat_sample_seq_freq.py
+++ reformat_sample_seq_freq.py
@@ -1,6 +1,14 @@
 import os
 import sys
 
+def get_file_content(filename):
+    # args = sys.argv
+    # filename = args[1]
+    fh = open(filename,'r')
+    all_lines = fh.readlines()
+    fh.close()
+    return all_lines
+
 my_dir = "/Users/ashipunova/work/emil/results_py"
 seq_tax_file_name = "seq_tax_u.txt"
 
@@ -18,24 +26,20 @@ seq_tax_dict = {}
 with open(seq_tax_file_name) as seq_tax:
   for line in seq_tax:
       listedline = line.strip().split('\t')
-      if len(listedline) > 1: # we have the = sign in there
+      if len(listedline) > 1: # we have the "\t" sign in there
           seq_tax_dict[listedline[0]] = listedline[1]
 
 print(seq_tax_dict)
 
-#
-# for file_name in file_names:
-#    file_name = os.path.join(my_dir, file_name)
-#
-#    # with open(full_name) as fp:
-#    line = fp.readline()
-#    cnt = 1
-#    while line:
-#        print("Line {}: {}".format(cnt, line.strip()))
-#        line = fp.readline()
-#        if (cnt == 1):
-#          get_headers
-#        cnt += 1
-#        headers = line.strip()
+all_dict = {}
+for file_name in file_names:
+  full_name = os.path.join(my_dir, file_name)
+  print(full_name)
+  content = get_file_content(full_name) 
+  cnt = 1
+  for line in content:
+     print("Line {}: {}".format(cnt, line.strip()))
+     cnt += 1
+  #        headers = line.strip()
 
 # Line 1: project__dataset  sequence_ill_id  sum_seq_count  taxonomy
