7123816f26ab7d9c55b7d4375a4dd10a97c609b8
annaship <tetya-loshad@yandex.ru>
2019-06-28 03:21:57 -0400

    all_dict

--- reformat_sample_seq_freq.py
+++ reformat_sample_seq_freq.py
@@ -38,6 +38,7 @@ file_names = ["test1.csv", "test2.csv"]
 
 
 all_dict = defaultdict()
+res_text = ""
 
 for file_name in file_names:
   full_name = os.path.join(my_dir, file_name)
@@ -48,6 +49,21 @@ for file_name in file_names:
      cnt += 1
      
      line_arr = line.strip().split()
-     # all_dict[]
      print("Line {}: {}".format(cnt, line_arr))
+     # Line 11: ['BBO_IGM_Bv4v5__R3_2', '118510832', '1', 'Bacteria;Firmicutes;Bacilli;Bacillales;Bacillaceae;Bacillus']
 
+     # all_dict[line_arr[0]]
+     pr_dat = line_arr[0]
+     seq = line_arr[1]
+     freq = line_arr[2]
+     # for k in seq_tax_dict.keys():
+       # print(k)
+       # res_text += line_arr[0] +
+     try:
+       all_dict[pr_dat][seq] = freq
+     except KeyError:         
+       all_dict[pr_dat] = {}
+       all_dict[pr_dat][seq] = freq
+     
+print(all_dict)
+# {'BBO_IGM_Bv4v5__G6_3': {'118510824': '1', '118510832': '1'}..., 'BBO_IGM_Bv4v5__R3_2': {'118510832': '1'}}
