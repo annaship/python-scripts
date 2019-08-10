0a158de62df0c5712ab8ab528b9e34ad175afe35
annaship <tetya-loshad@yandex.ru>
2019-08-10 13:54:10 -0400

    fix_freq

--- dada_res.py
+++ dada_res.py
@@ -10,17 +10,30 @@ def get_sequence_taxa(sequence_taxa_f_name):
   seq_t_d = {s: t for s, t in spls}
   return seq_t_d
 
+def fix_freq(next_n_lines):
+  res = []
+  n = 0
+  for l in next_n_lines:
+    n += 1
+    if n == 1:
+      res.append(l)
+    else:
+      res.append(l.replace(",", ";frequency="))
+
+  return res
+
 def get_dada_freq_res(freq_csv):
   # freq_csv = "/Users/ashipunova/work/dada2/stephen_fastq/seqtab.nochim.csv"
   freq_csv_f = open(freq_csv, "r")
   array_by4 = []
   n = 4
   while True:
-      next_n_lines = list(islice(freq_csv_f, n))
-      if not next_n_lines:
-              break
-      clean_lines = [l.strip("\n").strip(",") for l in next_n_lines]
-      array_by4.append(clean_lines)
+    next_n_lines = list(islice(freq_csv_f, n))
+    next_n_lines_f = fix_freq(next_n_lines)
+    if not next_n_lines_f:
+            break
+    clean_lines = [l.strip("\n").strip(",") for l in next_n_lines_f]
+    array_by4.append(clean_lines)
   return array_by4
 
 # array_by4 = ..., ['ATGCCGCGTGTATGAAGAAGGCCTTCGGGTTGTAAAGTACTTTCAGCGGGGAGGAAGGCGTTGAGGTTAATAACCTCAGCGATTGACGTTACCCGCAGAAGAAGCACCGGCTAACTCCGTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCACGCAGGCGGTCTGTCAAGTCGGATGTGAAATCCCCGGGCTTAACCTGGGAACTGCATTTGAAACTGGCAGGCTTGAGTCTCGTAGAGGGGGGTAGAATTCCAGGTGTAGCGGTGAAATGCGTAGAGATCTGGAGGAATACCGGTGGCGAAGGCGGCCCCCTGGACGAAGACTGACGCTCAGGTGCGAAAGCGTGGGGAGCAAACAGGATT\n', 'BP04-L-NCAP-16S;0\n', 'BP05-B-NCAP-16S;0\n', 'BP06-G-NCAP-16S;1\n']]
@@ -64,10 +77,11 @@ def write_fasta_from_dada_w_id(dict_seq_freq_by_seq_id):
     BP04_id = "%s|%s|%s" % (arr[2], i, arr[0]) 
     BP05_id = "%s|%s|%s" % (arr[3], i, arr[0]) 
     BP06_id = "%s|%s|%s" % (arr[4], i, arr[0]) 
-    f_out.write("%s#%s\n%s#%s\n%s#%s\n" % (BP04_id, arr[1], BP05_id, arr[1], BP06_id, arr[1]))
+    # f_out.write(">%s#%s\n%s#%s\n%s#%s\n" % (BP04_id, arr[1], BP05_id, arr[1], BP06_id, arr[1]))
+    fasta_template = ">%s\n%s\n"
+    f_out.write(fasta_template * 3 % (BP04_id, arr[1], BP05_id, arr[1], BP06_id, arr[1]))
   f_out.close()
-  
-  
+
 def write_fasta_from_dada(array_by4):
   # array_by4 = ..., ['ATGCCGCGTGTATGAAGAAGGCCTTCGGGTTGTAAAGTACTTTCAGCGGGGAGGAAGGCGTTGAGGTTAATAACCTCAGCGATTGACGTTACCCGCAGAAGAAGCACCGGCTAACTCCGTGCCAGCAGCCGCGGTAATACGGAGGGTGCAAGCGTTAATCGGAATTACTGGGCGTAAAGCGCACGCAGGCGGTCTGTCAAGTCGGATGTGAAATCCCCGGGCTTAACCTGGGAACTGCATTTGAAACTGGCAGGCTTGAGTCTCGTAGAGGGGGGTAGAATTCCAGGTGTAGCGGTGAAATGCGTAGAGATCTGGAGGAATACCGGTGGCGAAGGCGGCCCCCTGGACGAAGACTGACGCTCAGGTGCGAAAGCGTGGGGAGCAAACAGGATT\n', 'BP04-L-NCAP-16S;0\n', 'BP05-B-NCAP-16S;0\n', 'BP06-G-NCAP-16S;1\n']]
   f_out = open(os.path.join(out_file_dir, "freq_tax.fa"), "w")
@@ -123,9 +137,9 @@ BP06-G-NCAP-16S;1496
   
   # print(dict_seq_freq_by_seq_id)
   
-  BP04_L_NCAP_16S = {t: a[1].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
-  BP05_B_NCAP_16S = {t: a[2].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
-  BP06_G_NCAP_16S = {t: a[3].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
+  BP04_L_NCAP_ITS = {t: a[1].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
+  BP05_B_NCAP_ITS = {t: a[2].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
+  BP06_G_NCAP_ITS = {t: a[3].split(";")[1].strip("\n") for t, a in seq_freq_by_tax_d.items()}
 
   # write_out(BP04_L_NCAP_16S, "BP04_L_NCAP_16S")
   # write_out(BP05_B_NCAP_16S, "BP04_B_NCAP_16S")
