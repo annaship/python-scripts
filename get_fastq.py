7572a289f68b337cdcc99b612fa85c1d31b9716a
annaship <tetya-loshad@yandex.ru>
2019-09-19 16:46:25 -0400

    Update get_fastq.py
    use main

--- get_fastq.py
+++ get_fastq.py
@@ -27,42 +27,29 @@ def check_if_verb():
         return False
     return False
 
-start_dir = sys.argv[1]
-print("Start from %s" % start_dir)
-print("Getting file names")
-
-all_dirs = set()
-
-fq_files = get_files(start_dir, ".fastq.gz")
-print("Found %s fastq.gz files" % (len(fq_files)))
-
-check_if_verb = check_if_verb()
-
-for file_name in fq_files:
-    if (check_if_verb):
-        print(file_name)
-    log_f_name = "barcode_files.log"
-    log_f = open(log_f_name, "a")
-
-    try:
-        f_input = fq.FastQSource(file_name, True)
-        f_output = fq.FastQOutput(file_name + ".out")
-        print("len(f_input)")
-        while f_input.next(raw = True):
-            e = f_input.entry
-            e.sequence = e.sequence[5:]
-            e.qual_scores = e.qual_scores[5:]
-            f_output.store_entry(e)
-            log_f.write("%s: %s\n" % (file_name, e.sequence[0:5]))
-
-    except RuntimeError:
+def cut_barcodes():
+    for file_name in fq_files:
         if (check_if_verb):
-            print(sys.exc_info()[0])
-    except:
-        print("Unexpected error:", sys.exc_info())
-        next
+            print(file_name)
+        log_f_name = "barcode_files.log"
+        log_f = open(log_f_name, "a")
 
-print(all_dirs)
+        try:
+            f_input = fq.FastQSource(file_name, True)
+            f_output = fq.FastQOutput(file_name + ".out")
+            while f_input.next(raw = True):
+                e = f_input.entry
+                e.sequence = e.sequence[5:]
+                e.qual_scores = e.qual_scores[5:]
+                f_output.store_entry(e)
+                log_f.write("%s: %s\n" % (file_name, e.sequence[0:5]))
+
+        except RuntimeError:
+            if (check_if_verb):
+                print(sys.exc_info()[0])
+        except:
+            print("Unexpected error:", sys.exc_info())
+            next
 
 if __name__ == '__main__':
 
@@ -79,3 +66,16 @@ if __name__ == '__main__':
     print(args)
 
     is_verbatim = args.is_verbatim
+
+    # start_dir = sys.argv[1]
+    start_dir = args.start_dir
+    print("Start from %s" % start_dir)
+    print("Getting file names")
+
+    all_dirs = set()
+
+    fq_files = get_files(start_dir, ".fastq.gz")
+    print("Found %s fastq.gz files" % (len(fq_files)))
+
+    check_if_verb = check_if_verb()
+    cut_barcodes()
