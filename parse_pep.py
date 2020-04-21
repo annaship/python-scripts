128619b76f0d7a0df53f222d7894772fbedbf7fb
annaship <tetya-loshad@yandex.ru>
2020-04-21 14:37:21 -0400

    Update parse_pep.py

--- parse_pep.py
+++ parse_pep.py
@@ -12,7 +12,18 @@ class Pep:
     self.group_dict()
     self.good_res = []
     self.choose_entry()
+    self.out_txt = ""
+    self.form_res()
+
     # self.print_res()
+
+  def form_res(self):
+    for d in self.good_res:
+      all_arr = []
+      self.out_txt += "\n"
+      for k, v in d.items():
+        all_arr = all_arr + v
+      self.out_txt += "\n".join(all_arr)
 
   def print_res(self):
     for d in self.good_res:
@@ -24,16 +35,19 @@ class Pep:
 
   def choose_entry(self):
     for k, v_d in self.entries_dict.items():
-      if any(e.endswith("[Signal peptide detected]") for e in v_d['Signal']):
-        if any(e.startswith("Extracellular") for e in v_d['Final Prediction']):
-          self.good_res.append(v_d)
+      try:
+        if any(e.endswith("[Signal peptide detected]") for e in v_d['Signal']):
+          if any(e.startswith("Extracellular") for e in v_d['Final Prediction']):
+            self.good_res.append(v_d)
+      except KeyError:
+        pass
 
   def group_dict(self):
     for el in self.entries:
       temp_dict = {}
       try:
         self.entries_dict[el[0]] = {}
-        r1 = list(self.group(el, 'Signal+'))
+        r1 = list(self.group(el, 'Signal'))
         r2 = list(self.group(r1[1], 'Final Prediction:'))
         temp_dict["First part"] = r1[0]
         temp_dict["Signal"] = r2[0]
@@ -75,7 +89,5 @@ if __name__ == "__main__":
 
   pep = Pep(data)
 
-  with open(args.output_file, 'a') as f_output:
-    for d in pep.good_res:
-      for k, v in d.items():
-        f_output.write("\n".join(v))
+  with open(args.output_file, 'w') as f_output:
+        f_output.write(pep.out_txt)
