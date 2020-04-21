c03c6a49e8aa700023e016377b7b0c3c80d6b07a
annaship <tetya-loshad@yandex.ru>
2020-04-21 14:16:54 -0400

    Update parse_pep.py
    out

--- parse_pep.py
+++ parse_pep.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
 
 from collections import defaultdict
+import argparse
 
 
-class Pep():
+class Pep:
   def __init__(self, data):
     self.entries_dict = defaultdict()
     str_data = self.strip_n(data)
@@ -11,61 +12,36 @@ class Pep():
     self.group_dict()
     self.good_res = []
     self.choose_entry()
-    self.clean_res()
+    # self.print_res()
 
-  def clean_res(self):
-    pass
-    # for x in pep.entries_dict.values():
-    #   if any(e.startswith("Extracellular") for e in x['Final Prediction']):
-        # print(x['Final Prediction'])
-        # print(x)
+  def print_res(self):
+    for d in self.good_res:
+      for k, v in d.items():
+        print("\n".join(v))
 
   def strip_n(self, data):
-    return [l.strip() for l in data]
+    return [line.strip() for line in data]
 
   def choose_entry(self):
-    # temp_arr = []
     for k, v_d in self.entries_dict.items():
       if any(e.endswith("[Signal peptide detected]") for e in v_d['Signal']):
         if any(e.startswith("Extracellular") for e in v_d['Final Prediction']):
           self.good_res.append(v_d)
-        # [x for x in self.entries_dict.values() if any(e.startswith("Extracellular") for e in x['Final Prediction'])]
-        # self.good_res = [v for k, v in d.items() for d in temp_arr]
-
-    #       for d in pep.good_res:
-    #     for k, v in d.items():
-    #       print("\n".join(v))
-    # self.good_res = [x for x in self.entries_dict.values() if any(e.startswith("Extracellular") for e in x['Final Prediction'])]
-    # for x in pep.entries_dict.values():
-    #   if any(e.startswith("Extracellular") for e in x['Final Prediction']):
 
   def group_dict(self):
     for el in self.entries:
       temp_dict = {}
-      # print("TTTT")
       try:
-        # if el[0].startswith("SeqID"):
         self.entries_dict[el[0]] = {}
         r1 = list(self.group(el, 'Signal+'))
         r2 = list(self.group(r1[1], 'Final Prediction:'))
-        # temp_arr = r1[0] + r2
         temp_dict["First part"] = r1[0]
         temp_dict["Signal"] = r2[0]
         temp_dict["Final Prediction"] = r2[1]
         self.entries_dict[el[0]] = temp_dict
-        # self.entries_dict[el[0]].append(r2)
 
       except IndexError:
         pass
-      # if el.startswith(sep):
-      #   self.entries_dict[el.strip()] = []
-      # else:
-      #   temp_arr.append(el.strip())
-
-    #     yield temp_arr
-    #     temp_arr = []
-    #   temp_arr.append(el)
-    # yield temp_arr
 
   def group(self, seq, sep):
     temp_arr = []
@@ -78,14 +54,28 @@ class Pep():
 
 
 if __name__ == "__main__":
-  file_in = "/Users/ashipunova/work/emil/parse/file_in.txt"
-  file_out = "/Users/ashipunova/work/emil/parse/file_out.txt"
 
-  with open(file_in) as f_input:
+  parser = argparse.ArgumentParser(description = """Cuts the first 5 characters from sequences and quality lines.
+  Input: gzipped fastq files.
+  Output: the new shortened fastq entries and a log file with original file name and the cut 5nt sequences.""",
+                                   formatter_class = argparse.RawTextHelpFormatter)
+
+  parser.add_argument('-i', '--input_file',
+                      required = True, action = 'store', dest = 'input_file',
+                      help = """Input file name"""),
+
+  parser.add_argument("-o", "--output_file",
+                      required = True, action = "store", dest = "output_file",
+                      help = """Output file name""")
+
+  args = parser.parse_args()
+
+  with open(args.input_file) as f_input:
     data = f_input.readlines()
 
   pep = Pep(data)
 
-  for d in pep.good_res:
-    for k, v in d.items():
-      print("\n".join(v))
+  with open(args.output_file, 'a') as f_output:
+    for d in pep.good_res:
+      for k, v in d.items():
+        f_output.write("\n".join(v))
