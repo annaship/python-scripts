8b3ba8c9163ee184354e0208b071ef0fee9c90ff
annaship <tetya-loshad@yandex.ru>
2020-09-17 08:13:13 -0400

    upload into a seprate file. tsv_to_db works

--- mcm_upload_util.py
+++ mcm_upload_util.py
@@ -71,18 +71,32 @@ class Upload:
 
   foreign_key_tables = defaultdict(dict)
 
-  def __init__(self, utils):
+  def __init__(self, metadata):
     """
         1) upload simple tables (table_names_simple)
         2) upload combine tables no foreign keys (content, person, place, season, source)
         3) get ids
         4) upload tables with ids
     """
-    self.entry_table_name = self.table_names_w_ids[0]
-    all_tables_sql_res = mysql_utils.get_table_names(db_schema)
-    self.all_tables_set = set(utils.extract(all_tables_sql_res[0]))
+    self.utils = util.Utils()
+    self.metadata = metadata
 
-    self.many_values_to_one_field_column_names = utils.flatten_2d_list(self.many_values_to_one_field.values())
+    if self.utils.is_local():
+      self.db_schema = 'mcm_history'
+      self.mysql_utils = util.Mysql_util(host = 'localhost', db = self.db_schema, read_default_group = 'clienthome')
+      print("host = 'localhost', db = {}".format(self.db_schema))
+    else:
+      self.db_schema = 'mcmurdohistory_metadata'
+      host = '127.0.0.1'
+      self.mysql_utils = util.Mysql_util(host = host, db = self.db_schema, read_default_group = 'client')
+      # self.mysql_utils = util.Mysql_util(host = 'taylor.unm.edu', db = self.db_schema, read_default_group = 'client')
+      print("host = {}, db {}".format(host, self.db_schema))
+
+    self.entry_table_name = self.table_names_w_ids[0]
+    all_tables_sql_res = self.mysql_utils.get_table_names(self.db_schema)
+    self.all_tables_set = set(self.utils.extract(all_tables_sql_res[0]))
+
+    self.many_values_to_one_field_column_names = self.utils.flatten_2d_list(self.many_values_to_one_field.values())
     self.special_tables = self.get_special_tables()
     self.simple_tables = list(self.all_tables_set - set(self.special_tables))
 
@@ -103,7 +117,7 @@ class Upload:
 
   def drop_temp_table(self):
     drop_query = "DROP TABLE IF EXISTS {}".format(self.table_name_temp_dump)
-    mysql_utils.execute_no_fetch(drop_query)
+    self.mysql_utils.execute_no_fetch(drop_query)
 
   def create_temp_table(self):
     create_table_q = """
@@ -113,9 +127,9 @@ class Upload:
       `updated` datetime DEFAULT NULL
     ) ENGINE=InnoDB;
     """.format(self.table_name_temp_dump)
-    mysql_utils.execute_no_fetch(create_table_q)
+    self.mysql_utils.execute_no_fetch(create_table_q)
 
-    all_fields = metadata.metadata_to_field.values()
+    all_fields = self.metadata.metadata_to_field.values()
     column_names_w_ids = [x + "_id" for x in all_fields]
     #           ADD COLUMN `ping_status` INT(1) NOT NULL AFTER
     #   `bibliographic_citation` varchar(512) DEFAULT '',
@@ -138,7 +152,7 @@ class Upload:
     column_names_str = ", ".join(column_names_arr)
     add_columns_q = column_names_str_begin + column_names_str
     # print(add_columns_q)
-    mysql_utils.execute_no_fetch(add_columns_q)
+    self.mysql_utils.execute_no_fetch(add_columns_q)
 
     # print("QQ")
 
@@ -152,7 +166,7 @@ class Upload:
     self.all_tables_set.discard(self.table_names_to_ignore[0])
     for table_name in list(self.all_tables_set):
       insert_query = "INSERT IGNORE INTO `{}` (`{}`) VALUES (NULL)".format(table_name, table_name + "_id")
-      mysql_utils.execute_no_fetch(insert_query)
+      self.mysql_utils.execute_no_fetch(insert_query)
 
   def upload_simple_tables(self):
     for table_name in self.simple_tables:
@@ -163,30 +177,30 @@ class Upload:
     """
     try:
       if not val_arr:
-        val_arr = list(set(metadata.not_empty_tsv_content_dict[field_name]))
-      mysql_utils.execute_insert_many(table_name, field_name, val_arr)
+        val_arr = list(set(self.metadata.not_empty_tsv_content_dict[field_name]))
+      self.mysql_utils.execute_insert_many(table_name, field_name, val_arr)
 
     except KeyError:
       insert_query = "INSERT IGNORE INTO `{}` (`{}`) VALUES (NULL)".format(table_name, table_name + "_id")
-      mysql_utils.execute_no_fetch(insert_query)
+      self.mysql_utils.execute_no_fetch(insert_query)
 
   def upload_all_from_tsv_into_temp_table(self):
     table_name = self.table_name_temp_dump
     table_name_id = table_name + "_id"
-    for current_row_d in metadata.tsv_file_content_dict_clean_keys:
+    for current_row_d in self.metadata.tsv_file_content_dict_clean_keys:
       field_names_arr = list(current_row_d.keys())
       values_arr      = list(current_row_d.values())
 
-      mysql_utils.execute_many_fields_one_record(table_name, field_names_arr, tuple(values_arr))
+      self.mysql_utils.execute_many_fields_one_record(table_name, field_names_arr, tuple(values_arr))
 
       # separate as add_id_back
-      current_id = mysql_utils.get_id_esc(table_name_id, table_name, field_names_arr, values_arr)
+      current_id = self.mysql_utils.get_id_esc(table_name_id, table_name, field_names_arr, values_arr)
       current_row_d[table_name_id] = current_id
 
   def mass_update_simple_ids(self):
     for table_name in self.simple_tables:
       try:
-        current_vals = list(set(metadata.not_empty_tsv_content_dict[table_name]))
+        current_vals = list(set(self.metadata.not_empty_tsv_content_dict[table_name]))
       except KeyError:
         # is empty
         continue
@@ -196,28 +210,28 @@ class Upload:
       templ = ", ".join(templ_arr)
       select_q = """SELECT {}, {} FROM {} WHERE {} in ({});
       """.format(field_name, field_name_id, table_name, field_name, templ)
-      sql_res = mysql_utils.execute_fetch_select(select_q, current_vals)
+      sql_res = self.mysql_utils.execute_fetch_select(select_q, current_vals)
 
       for (val, val_id) in sql_res[0]:
         update_q = '''UPDATE {}
           SET {} = {} 
           WHERE {} = %s'''.format(self.table_name_temp_dump, field_name_id, val_id, field_name)
-        mysql_utils.execute_no_fetch(update_q, val)
+        self.mysql_utils.execute_no_fetch(update_q, val)
 
   def upload_many_values_to_one_field(self):
     tsv_field_names_to_upload = self.many_values_to_one_field_column_names
-    value_present = utils.intersection(tsv_field_names_to_upload, metadata.not_empty_tsv_content_dict.keys())
+    value_present = self.utils.intersection(tsv_field_names_to_upload, self.metadata.not_empty_tsv_content_dict.keys())
 
     for table_name, tsv_field_names in self.many_values_to_one_field.items():
       for tsv_field_name in tsv_field_names:
         if tsv_field_name in value_present:
-          val_arr = list(set(metadata.not_empty_tsv_content_dict[tsv_field_name]))
+          val_arr = list(set(self.metadata.not_empty_tsv_content_dict[tsv_field_name]))
           self.simple_mass_upload(table_name, table_name, val_arr)
 
   def update_many_values_to_one_field_ids(self):
     table_name_to_update = self.table_name_temp_dump
-    tsv_field_names_to_upload = utils.flatten_2d_list(self.many_values_to_one_field.values())
-    for current_row_d in metadata.tsv_file_content_dict_clean_keys:
+    tsv_field_names_to_upload = self.utils.flatten_2d_list(self.many_values_to_one_field.values())
+    for current_row_d in self.metadata.tsv_file_content_dict_clean_keys:
       """TODO: go over each table values instead?     
       for table_name, tsv_field_names in self.many_values_to_one_field.items():"""
       for tsv_field_name in tsv_field_names_to_upload:
@@ -226,11 +240,11 @@ class Upload:
         except KeyError:
           continue
         table_name_w_id = self.where_to_look_if_not_the_same[tsv_field_name]
-        current_id = mysql_utils.get_id_esc(table_name_w_id + '_id', table_name_w_id, table_name_w_id, current_value)
+        current_id = self.mysql_utils.get_id_esc(table_name_w_id + '_id', table_name_w_id, table_name_w_id, current_value)
 
         # TODO: update these in columns rather then in rows (all data_exact where == 1976 etc.)
         update_q = 'UPDATE {} SET {} = {} WHERE {} = %s'.format(table_name_to_update, tsv_field_name + '_id', current_id, tsv_field_name)
-        mysql_utils.execute_no_fetch(update_q, current_value)
+        self.mysql_utils.execute_no_fetch(update_q, current_value)
 
   def get_entry_table_field_names(self):
     entry_field_names_q = """
@@ -240,15 +254,15 @@ class Upload:
       AND table_schema = %s 
       AND column_name <> %s
     """
-    vals = (self.entry_table_name, db_schema, self.entry_table_name + "_id")
-    return mysql_utils.execute_fetch_select(entry_field_names_q, vals)
+    vals = (self.entry_table_name, self.db_schema, self.entry_table_name + "_id")
+    return self.mysql_utils.execute_fetch_select(entry_field_names_q, vals)
 
   def get_empty_field_names(self, current_row_dict):
     except_fields = ["created", "updated"]
     entry_field_names_sql_res = self.get_entry_table_field_names()
     have_field_names = [k for k, v in current_row_dict.items() if v > 0]
     # TODO: seems slow, benchmark and try with utils.subtraction
-    res = list(set(utils.extract(entry_field_names_sql_res[0])) - set(have_field_names) - set(except_fields))
+    res = list(set(self.utils.extract(entry_field_names_sql_res[0])) - set(have_field_names) - set(except_fields))
     return res
 
   def find_empty_ids(self, current_row_dict):
@@ -263,57 +277,45 @@ class Upload:
       try:
         table_name_w_id = self.where_to_look_if_not_the_same[name_no_id]
         #
-        # empty_id = mysql_utils.execute_fetch_select(select_q)
-        # current_row_dict[field] = list(utils.extract(empty_id))[0]
+        # empty_id = self.mysql_utils.execute_fetch_select(select_q)
+        # current_row_dict[field] = list (self.utils.extract(empty_id))[0]
       except KeyError:
         table_name_w_id = name_no_id
 
       select_q = 'SELECT {} FROM {} WHERE {} = ""'.format(table_name_w_id + "_id", table_name_w_id, table_name_w_id)
-      empty_id = mysql_utils.execute_fetch_select(select_q)
-      current_row_dict[field] = list(utils.extract(empty_id))[0]
+      empty_id = self.mysql_utils.execute_fetch_select(select_q)
+      current_row_dict[field] = list (self.utils.extract(empty_id))[0]
 
     return current_row_dict
 
   def upload_other_tables(self):
     table_name_to_update = self.entry_table_name # ["entry"]
     where_to_look_for_ids = self.table_name_temp_dump
-    for current_row_d in metadata.tsv_file_content_dict_clean_keys:
+    for current_row_d in self.metadata.tsv_file_content_dict_clean_keys:
       tsv_field_names_to_upload = current_row_d.keys()
       tsv_field_names_to_upload_ids = [x+"_id" for x in tsv_field_names_to_upload if not x.endswith("_id")]
       tsv_field_names_to_upload_ids_str = ', '.join(tsv_field_names_to_upload_ids)
 
       unique_keys = current_row_d.keys()
-      where_part0 = mysql_utils.make_where_part_template(unique_keys)
+      where_part0 = self.mysql_utils.make_where_part_template(unique_keys)
 
       select_q = '''SELECT {} FROM {} 
         WHERE {}'''.format(tsv_field_names_to_upload_ids_str, where_to_look_for_ids, where_part0)
-      sql_res = mysql_utils.execute_fetch_select_to_dict(select_q, current_row_d.values())
+      sql_res = self.mysql_utils.execute_fetch_select_to_dict(select_q, current_row_d.values())
       dict_w_all_ids = self.find_empty_ids(sql_res[0])
       # IF empty and no id - get it
-      mysql_utils.execute_many_fields_one_record(table_name_to_update, list(dict_w_all_ids.keys()), tuple(dict_w_all_ids.values()))
+      self.mysql_utils.execute_many_fields_one_record(table_name_to_update, list(dict_w_all_ids.keys()), tuple(dict_w_all_ids.values()))
 
 
 if __name__ == '__main__':
   # /Users/ashipunova/work/MCM/mysql_schema/Bibliography_test.csv
 
-  utils = util.Utils()
-
-  if utils.is_local():
-    db_schema = 'mcm_history'
-    mysql_utils = util.Mysql_util(host = 'localhost', db = db_schema, read_default_group = 'clienthome')
-    print("host = 'localhost', db = {}".format(db_schema))
-  else:
-    db_schema = 'mcmurdohistory_metadata'
-    host = '127.0.0.1'
-    mysql_utils = util.Mysql_util(host = host, db = db_schema, read_default_group = 'client')
-    # mysql_utils = util.Mysql_util(host = 'taylor.unm.edu', db = db_schema, read_default_group = 'client')
-    print("host = {}, db {}".format(host, db_schema))
-
+  pass
   # upload = upload_tsv_to_db_for_mcm.Upload(utils)
 
-  c = Collections()
-  export = Export()
-  import_to_mysql = ToMysql()
+  # c = Collections()
+  # export = Export()
+  # import_to_mysql = ToMysql()
   # export.()
   # export.print_items_info()
   # export.all_items_fields()
