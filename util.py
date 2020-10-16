63948bb3ad6ff15dfba23978c2f28c60dd1bb5ca
annaship <tetya-loshad@yandex.ru>
2020-10-16 11:30:32 -0400

    Update util.py
    flatten dict

--- util.py
+++ util.py
@@ -13,7 +13,8 @@ except ImportError:
 import timeit
 import time
 import csv
-from collections import Iterable
+# from collections import Iterable
+import collections
 
 import datetime
 import logging
@@ -508,6 +509,23 @@ class Utils:
     res_str = self.make_quoted_str(my_list)
     return res_str
 
+  def flatten_dict(self, d, sep = "_"):
+    obj = collections.OrderedDict()
+
+    def recurse(t, parent_key = ""):
+
+      if isinstance(t, list):
+        for i in range(len(t)):
+          recurse(t[i], parent_key + sep + str(i) if parent_key else str(i))
+      elif isinstance(t, dict):
+        for k, v in t.items():
+          recurse(v, parent_key + sep + k if parent_key else k)
+      else:
+        obj[parent_key] = t
+
+    recurse(d)
+    return obj
+
   def flatten_2d_list(self, my_list):
     return [item for sublist in my_list for item in sublist]
 
@@ -517,7 +535,7 @@ class Utils:
 
   def flatten(self, collection):
     for x in collection:
-      if isinstance(x, Iterable) and not isinstance(x, str):
+      if isinstance(x, collections.Iterable) and not isinstance(x, str):
         yield from self.flatten(x)
       else:
         yield x
