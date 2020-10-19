22923602502fd8b43b07fa3fe16cf5562b981b26
annaship <tetya-loshad@yandex.ru>
2020-10-19 18:22:23 -0400

    Update mcm_upload_util.py
    does not update

--- mcm_upload_util.py
+++ mcm_upload_util.py
@@ -315,24 +315,26 @@ class Upload:
   def check_if_id_is_in_entry(self, current_id):
     """
     :param current_id:
-    :return:
-
-    SELECT * FROM entry JOIN identifier USING(identifier_id)
-WHERE identifier = "MCMEH-B0000023"
-limit 1;
+    :return: boolean
     """
     identifier_table_name = self.metadata.data_managing.identifier_table_name
     query = """
-    SELECT * FROM {0} JOIN {1} USING({2})
-WHERE {1} = "MCMEH-B0000023"
-limit 1;""".format(self.entry_table_name, identifier_table_name, identifier_table_name + "_id")
-    print(query)
+    SELECT * FROM {0} JOIN {1} USING({1}_id)
+WHERE {1} = %s
+limit 1;""".format(self.entry_table_name, identifier_table_name)
+    # print(query)
+    res = self.mysql_utils.execute_fetch_select(query, current_id)
+    id_is_in_entry = False
+    if len(res[0]) > 0:
+      id_is_in_entry = True
+    return id_is_in_entry
+
 
   def upload_other_tables(self):
     table_name_to_update = self.entry_table_name # ["entry"]
     where_to_look_for_ids = self.table_name_temp_dump
     for current_row_d in self.metadata.tsv_file_content_dict_ok:
-      self.check_if_id_is_in_entry(current_row_d['identifier'])
+      id_is_in_entry = self.check_if_id_is_in_entry(current_row_d[self.metadata.data_managing.identifier_table_name ])
       tsv_field_names_to_upload = current_row_d.keys()
       tsv_field_names_to_upload_ids = [x+"_id" for x in tsv_field_names_to_upload if not x.endswith("_id")]
       tsv_field_names_to_upload_ids_str = ', '.join(tsv_field_names_to_upload_ids)
@@ -345,7 +347,16 @@ limit 1;""".format(self.entry_table_name, identifier_table_name, identifier_tabl
       sql_res = self.mysql_utils.execute_fetch_select_to_dict(select_q, current_row_d.values())
       dict_w_all_ids = self.find_empty_ids(sql_res[0])
       # IF empty and no id - get it
-      self.mysql_utils.execute_many_fields_one_record(table_name_to_update, list(dict_w_all_ids.keys()), tuple(dict_w_all_ids.values()))
+
+      # TODO: why diff from tsv_field_names_to_upload_ids?
+      all_fields = list(dict_w_all_ids.keys()) #had to be done again to remove a fields
+      fields_to_update = ["{0} = @{0}".format(field_name) for field_name in all_fields]
+      fields_to_update_str = ", ".join(fields_to_update)
+
+      q_addition = ""
+      if id_is_in_entry:
+        q_addition = """ ON DUPLICATE KEY UPDATE {}""".format(fields_to_update_str)
+      self.mysql_utils.execute_many_fields_one_record(table_name_to_update, all_fields, tuple(dict_w_all_ids.values()), ignore = "IGNORE", addition = q_addition)
 
 
 class File_retrival:
